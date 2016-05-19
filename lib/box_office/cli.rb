8c4ba1bdb3dd12ad0a63c3b4bc6ad3d33c54442d
empireofryan <rjohnson2001@gmail.com>
2016-05-19 12:51:46 -0400

    refactor class

--- lib/box_office/cli.rb
+++ lib/box_office/cli.rb
@@ -7,8 +7,8 @@ class BoxOffice::CLI
   end
 
   def list_gross
-    @titles = BoxOffice::Gross.get_movie_titles
-    # binding.pry
+    @titles = BoxOffice::Movie.get_movie_titles
+
     @titles.each.with_index(1) do |title, i|
       puts "#{i}. #{title}"
     end
@@ -23,32 +23,30 @@ class BoxOffice::CLI
         case input
         when "1"
           puts "More info on movie 1..."
-          @gross = BoxOffice::Gross.scrape_grosses_1
+          @gross = BoxOffice::Movie.scrape_grosses_1
           puts "Made a whopping #{@gross} over the weekend!"
         when "2"
           puts "More info on movie 2..."
-          @gross = BoxOffice::Gross.scrape_grosses_2
+          @gross = BoxOffice::Movie.scrape_grosses_2
           puts "Rang up an impressive #{@gross} over the weekend!"
         when "3"
           puts "More info on movie 3..."
-          @gross = BoxOffice::Gross.scrape_grosses_3
+          @gross = BoxOffice::Movie.scrape_grosses_3
           puts "Cashing in to the tune of #{@gross} this weekend."
         when "4"
           puts "More info on movie 4..."
-          @gross = BoxOffice::Gross.scrape_grosses_4
+          @gross = BoxOffice::Movie.scrape_grosses_4
           puts "Chi-ching - that's the sound of #{@gross} this weekend."
         when "5"
           puts "More info on movie 5..."
-          @gross = BoxOffice::Gross.scrape_grosses_5
-          # @theaters = BoxOffice::Gross.scrape_theaters_5
+          @gross = BoxOffice::Movie.scrape_grosses_5
           puts "Clocking in at #{@gross} for the weekend."
-          # puts "Weekend Gross - #{@gross} - Theaters - #{@theaters}"
         end
 
       elsif input == 'list'
         list_gross
       elsif input == 'exit'
-        puts 'See you later Alligator'
+        puts 'See you on the silver screen!'
       else 
         puts "Not sure what you want, type list or exit"
       end
--- lib/box_office/gross.rb
+++ lib/box_office/gross.rb
@@ -1,5 +1,5 @@
 require 'pry'
-class BoxOffice::Gross # or maybe Movie?
+class BoxOffice::Movie
 
   attr_accessor :title
 
@@ -82,94 +82,4 @@ class BoxOffice::Gross # or maybe Movie?
 
   end
 
-  def self.scrape_theaters_5
-    doc = Nokogiri::HTML(open("http://www.fandango.com/boxoffice"))
-    theaters = self.new
-
-    theaters = doc.css("td.movieTitle + td + td").text
-
-    theaters = theaters.split("\n")
-    # theaters.select!{|a| a.exclude?("                                    ")}
-    # theaters.select!{|a| a.include?("=~ /^[-+]?[1-9]([0-9]*)?$/")}
-    theaters[4].strip
-
-  end
-
 end
-
-  # def self.scrape_grosses_1
-  #   doc = Nokogiri::HTML(open("http://www.boxofficemojo.com/daily/chart/"))
-  #   gross = self.new
-
-  #   gross = doc.css("table tr td table tr td b a").first.text
-    
-
-  #   gross
-
-  # end
-
-    # grosses = []
-    #  movie.gross = doc.css("table tr td table tr td b p").each do |e| 
-    #   grosses << e.text 
-    # end
-
-# 1. The jungle Book - 40 mil
-# 2. the huntsman - 30 mil
-# 3. barbershop - 20 mil
-
-    # movie.title = doc.xpath("/html/body/div[@id='container']/div[@id='main']/div[@id='body']/center/table/tbody/tr[2]/td/table/tbody/tr[2]/td[2]/b/a").text
-
-
-  # movie.boxoffice = doc.css("table tr td table tr td").first.attr("text")
-
-# movie.title = doc.css('table')[8].css('tr').each do |mov|
-#   new_movie = mov.css('td').each do |td|
-#     td.text
-#   end
-#   new_movie.save
-# end
-
-#     doc.css('table')[8].css('tr').each do |mov|
-# # HERE WE create a new Movie.new
-#      new_movie = mov.css('td').each do |td|
-# #Possibly here (or we can hard code it) we want to assign values to that new movie created above
-#       td.text
-#       end
-#       new_movie.save
-# #here we want to save that movie so the next iteration is a new movie object.
-#     end
-
-#movie.title = doc.css("table tr td table tr td b a").text
-
-    # second_movie = doc.css("table tr td table tr td b a").first(2)
-
-    # binding.pry
-
-    #url = doc.search("a.wantone").first.attr("href")
-
-
-# title = doc.search('table.tbody.tr.td.b.a').text
-# first_movie = doc.css("table tr td table tr td b a").first.text
-
-# second_movie = doc.css("table tr td table tr td b a").first(2)
-
-# first_movie = doc.css("table tr td table tr td b a")
-
-#old code
-
-    # should return a bunch of instances of grosses
-    # puts "Today's Box Office Grosses:"
-    # puts <<-DOC
-    #   1. Jungle Book
-    #   2. Star Wars
-    # DOC
-
-    # gross_1 = self.new
-    # gross_1.name = "Jungle Book"
-    # gross_1.boxoffice = "$21.5"
-
-    # gross_2 = self.new
-    # gross_2.name = "Star Wars"
-    # gross_2.boxoffice = "$14.2"
-
-    # [gross_1, gross_2]
