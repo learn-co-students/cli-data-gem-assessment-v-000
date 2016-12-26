b32c365082887c14b241c0da5f1c9ba456dea165
Pamela Bortnick <pbortnick@gmail.com>
2016-12-26 16:52:00 -0500

    Update recipes.rb
    edited scrape_r_1

--- pb-recipe/lib/recipe/recipes.rb
+++ pb-recipe/lib/recipe/recipes.rb
@@ -1,6 +1,6 @@
 class Recipe::Recipes
 
-  attr_accessor :time, :servings, :ingredients, :Instructions
+  attr_accessor :time, :servings, :ingredients, :instructions
 
   def self.scrape_recipes
     #Returns array of recipe information for dish 1 and dish 2
@@ -16,8 +16,8 @@ class Recipe::Recipes
     r_info = self.new
     r_info.time = r_page.search("div.cooking-times dd.total").first.text
     r_info.servings = r_page.search("div.difficulty dd").first.text.strip
-    r_info.ingredients = r_page.search("div.col8 ingredients ul").text.strip
-    r_info.instructions = r_page.search("div.col10 directions ul").text.strip
+    r_info.ingredients = r_page.search("div.col8.ingredients li").text
+    r_info.instructions = r_page.search("ul.recipe-directions-list").text.strip
     r_info
   end
 
@@ -26,8 +26,8 @@ class Recipe::Recipes
     r_page = Nokogiri::HTML(open("http://www.mydailymoment.com/recipes/easy_brownies.php"))
     r_info = self.new
     r_info.time = r_page.search("div.rr_commenttext p").text.strip
-    r_info.servings = r_page.search("div.rr_infodata").text.strip
-    r_info.ingredients = r_page.search("div.rr_introtext div.ingredient li").text.strip
+    r_info.servings = r_page.search("div.rr_infodata").last.text.strip
+    r_info.ingredients = r_page.search("div.rr_introtext div.ingredient li").text
     r_info.instructions = r_page.search("div.instructions ol.rr_method_list li").text.strip
     r_info
   end
