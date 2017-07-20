55be35e61461f34f182b95df9aac388ca0fab29e
Catherine Cordova <ccordova41@gmail.com>
2017-07-20 19:51:12 +0000

    Done.

---
+++ best_companies/lib/best_companies.rb
@@ -0,0 +1,2 @@
+require_relative "./best_companies/version"
+require_relative './best_companies/cli'
---
+++ best_companies/lib/best_companies/cli.rb
@@ -0,0 +1,8 @@
+#CLI controller
+
+ class BestCompanies::CLI
+
+   def call
+     puts "This year's Best Companies to work for: "
+   end
+ end
