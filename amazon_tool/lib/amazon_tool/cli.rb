2f5b21d173f92f8b5790f43b8cf518e817be6971
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-19 06:43:10 +0000

    Done.

--- amazon_tool/lib/amazon_tool/cli.rb
+++ amazon_tool/lib/amazon_tool/cli.rb
@@ -19,17 +19,42 @@ class AmazonTool::CLI
       6. Clothing, Shoes & Jewelry
     DOC
 
+    new_response = gets.strip
+
+    case new_response
+    when "1"
+      puts "Toys & Games:"
+    when "2"
+      puts "Electronics"
+    when "3"
+      puts "Camera & Photo"
+    when "4"
+      puts "Video Games"
+    when "5"
+      puts "Books"
+    when "6"
+      puts "Clothing, Shoes & Jewelry"
+    when "menu"
+      menu
+    when "products"
+      list_products
+    else
+      puts "Not sure what you're wanting to do. Type products, or menu"
+    end
+
+
+
   end
 
   def menu
     puts <<-DOC.gsub /^\s*/, ''
-    1.  Show Amazon best sellers(hourly), by category.
+    1.  Show Amazon best sellers, by category.
     2.  Generate Review
-    3.  Find Sales Rank
+    3.  Find Sales Rank of Product
     DOC
 
     response = nil
-    while response != "Exit"
+    while response != "exit"
         response = gets.strip.downcase
         case response
         when "1"
@@ -38,6 +63,8 @@ class AmazonTool::CLI
           puts "generate_review"
         when "3"
           puts "sales_rank"
+        when "menu"
+          menu
         else
           puts "Not sure what you're wanting to do. Type Menu, or Exit"
         end
