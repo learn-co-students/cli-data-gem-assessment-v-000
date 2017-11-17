7c1b34e7874be2ac9f25fd4ccf513d22a2e59345
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-17 23:42:00 +0000

    Done.

--- wine-under-20/lib/wine_under_20.rb
+++ wine-under-20/lib/wine_under_20.rb
@@ -1,5 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
-require_relative './wine_under_20/cli.rb'
-require_relative './wine_under_20/wine.rb'
+
+require_relative "./wine_under_20/version"
+require_relative './wine_under_20/wine'
+require_relative './wine_under_20/cli'
