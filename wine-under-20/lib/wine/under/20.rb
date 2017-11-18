1d11726bd5ef74c04cec2d2af70ecf4f42db4bf1
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-18 17:13:07 +0000

    Done.

--- wine-under-20/lib/wine/under/20.rb
+++
@@ -1,9 +0,0 @@
-require "wine/under/20/version"
-
-module Wine
-  module Under
-    module 20
-      # Your code goes here...
-    end
-  end
-end
--- wine-under-20/lib/wine_under_20/wine.rb
+++ wine-under-20/lib/wine_under_20/wine.rb
@@ -1,49 +1,37 @@
 class WineUnder20::Wine
-<<<<<<< HEAD
-  attr_accessor :name, :price
-=======
-  attr_accessor :name, :price, :url
->>>>>>> 7e52c4464d65f0a0fb5a728fea59233b3561aee1
+  attr_accessor :name, :price, :url, :description
 
-  def self.today
-    self.scrape_wines
-  end
 
-  def self.scrape_wines
-    wines = []
+#move this to a scraper class
+  def scrape_wines
+    @doc = Nokogiri::HTML(open("http://www.binnys.com/wine"))
+    @doc.search("div.row.result").collect do |wine_div|
+      wine = WineUnder20::Wine.new
+      wine.url = wine_div.search
+      wine.name = wine_div.search
+      wine.price = wine_div.search
+      wine.description = wine_div.search
+    end
 
-    wines << self.scrape_wine_site
-    #Go to wine website, find the products.
-    #extract the properties
-    #instantiate a wine
-    # create an array of wines based on zip code
-    wines
   end
 
   def self.scrape_wine_site
-<<<<<<< HEAD
     wine = self.new
-    doc = Nokogiri::HTML(open("http://www.binnys.com/wine"))
+
     #binding.pry
 # @doc.search each do |wine|
-
 #name =
 #price =
 
 #end
-
     wine.name = doc.css("h3.title").text
     wine.price = doc.css("div.prodPrice.unitPrice.salePrice").text
     wine
-=======
-    doc = Nokogiri::HTML(open(insertsitehere))
-    binding.pry
-    name =
-    price =
-    url =
+  #  binding.pry
+  #  name =
+  #  price =
+  #  url =
     #zip_code
-  end
->>>>>>> 7e52c4464d65f0a0fb5a728fea59233b3561aee1
   end
 
 end
