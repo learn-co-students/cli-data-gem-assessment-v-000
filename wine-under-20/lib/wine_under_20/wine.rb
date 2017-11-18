862be08e1c5fc319ac4c6b3a213f8f7170dc881d
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-18 18:03:38 +0000

    Done.

--- wine-under-20/lib/wine_under_20/wine.rb
+++ wine-under-20/lib/wine_under_20/wine.rb
@@ -10,7 +10,7 @@ class WineUnder20::Wine
       wine = WineUnder20::Wine.new
       # wine.url = wine_div.search().attr("href")
       wine.name = wine_div.search("h3.title").text.strip
-      wine.price = wine_div.search("div.prodPrice.unitPrice.salePrice").text.gsub("(Reg:$/./)", "").strip
+      wine.price = wine_div.search("div.prodPrice.unitPrice.salePrice").text.sub /\s*\(.+\)$/, ''.strip
       wine.description = wine_div.search("div.shortreview.p.a").text.strip
 
       wines << wine
