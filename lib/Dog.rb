c47eee5b2fcfd7d1885c77eb23872f634869a81e
Blakeley Bond <burrick2003@yahoo.com>
2017-12-06 04:38:31 +0000

    Done.

--- lib/Dog.rb
+++ lib/Dog.rb
@@ -1,5 +1,10 @@
 class Dog
-
-
+attr_accessor :gender, :breed, :age, :color, :size, :housebroken, :obedience, :location, :kids, :cats, :otherdogs, :sponsors, :description, :id, :imageurl
+def initialize(dog_hash) #from Mass Assignment & Metaprogramming Lab
+    dog_hash.each do |key, value|
+      self.send(("#{key}="), value)
+    @@all << self
+    end
+  end
 
 end #DOG
---
+++ lib/retriever.rb
@@ -0,0 +1,11 @@
+class Retriever
+
+
+
+
+
+
+
+
+
+end #RETRIEVER
