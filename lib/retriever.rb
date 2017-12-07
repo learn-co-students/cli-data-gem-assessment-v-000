b80f5981427e74101616a3d6aeebeffd5d91eaf1
Blakeley Bond <burrick2003@yahoo.com>
2017-12-07 03:01:45 +0000

    Done.

--- lib/retriever.rb
+++ lib/retriever.rb
@@ -1,9 +1,13 @@
 class Retriever
   def self.dog_scrape(adopt_url)
     html = open(adopt_url)
-
+    dogs_page = Nokogiri::HTML(html)
+    dogs = []
+        dogs_page.css("div.panel-2col-stacked.clearfix.panel-display").each do |dog_frame|
+          binding.pry
+        end
   end #dog_scrape
 
 
-
+dogs
 end #Retriever
