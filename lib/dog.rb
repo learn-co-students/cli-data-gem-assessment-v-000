a74e5bd84e278f7b6000fdb36a849118e049d30a
Blakeley Bond <burrick2003@yahoo.com>
2017-12-07 02:03:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 class Dog
-attr_accessor :gender, :breed, :age, :color, :size, :housebroken, :obedience, :location, :kids, :cats, :otherdogs, :sponsors, :description, :id, :imageurl
+attr_accessor :gender, :breed, :age, :color, :size, :housebroken, :obedience, :location, :kids, :cats, :otherdogs, :sponsors, :sponsor_blurb, :sponsor_name, :sponsor_date :description, :id, :imageurl
+
 def initialize(dog_hash) #from Mass Assignment & Metaprogramming Lab
     dog_hash.each do |key, value|
       self.send(("#{key}="), value)
--- lib/retriever.rb
+++ lib/retriever.rb
@@ -2,7 +2,14 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri'
 
-class Retriever
+#takes in a URL page of adoptable MAGSR format and scrapes for .Dog attributes
+class Retriever(adopt_page)
+  html = File.read(adopt_page)
+  dogs_html = Nokogiri::HTML(html)
+  dogs = []
+      dogs.html.css("div.panel-2col-stacked.clearfix.panel-display").each do |dog_frame|
+      end
+
 
 
 
