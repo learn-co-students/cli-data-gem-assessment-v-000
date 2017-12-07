cca7d76c3cba01008abbcc9b995c3df25fb4f881
Blakeley Bond <burrick2003@yahoo.com>
2017-12-07 03:23:03 +0000

    Done.

--- lib/retriever.rb
+++ lib/retriever.rb
@@ -4,10 +4,13 @@ class Retriever
     dogs_page = Nokogiri::HTML(html)
     dogs = []
         dogs_page.css("div.panel-2col-stacked.clearfix.panel-display").each do |dog_frame|
+          :name = dog_frame.css("h1.views-field.views-field-field-dog-name.dog-name span.field-content").text
+
           binding.pry
         end
+    dogs
   end #dog_scrape
 
 
-dogs
+
 end #Retriever
---
+++ temp.rb
@@ -0,0 +1,2 @@
+dog name
+("h1.views-field.views-field-field-dog-name.dog-name span.field-content").text
