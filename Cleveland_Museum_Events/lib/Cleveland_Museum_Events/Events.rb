25503cdca00d8cd1bc06009d55ff2e7197db1050
Steven Senkowski <scsenkowski@gmail.com>
2017-12-12 15:31:11 +0000

    added spec.md file to root directory

--- Cleveland_Museum_Events/lib/Cleveland_Museum_Events/Events.rb
+++ Cleveland_Museum_Events/lib/Cleveland_Museum_Events/Events.rb
@@ -1,6 +1,14 @@
 require "Cleveland/Museum/Events/version"
 
 module Cleveland
+<<<<<<< HEAD
 
       # Your code goes here...
+=======
+  module Museum
+    module Events
+      # Your code goes here...
+    end
+  end
+>>>>>>> ddd398c8c550e1fdd4b161e24891506844c82b8f
 end
--- Cleveland_Museum_Events/lib/Cleveland_Museum_Events/version.rb
+++ Cleveland_Museum_Events/lib/Cleveland_Museum_Events/version.rb
@@ -1,4 +1,13 @@
 module Cleveland
+<<<<<<< HEAD
 
       VERSION = "0.1.0"
     end
+=======
+  module Museum
+    module Events
+      VERSION = "0.1.0"
+    end
+  end
+end
+>>>>>>> ddd398c8c550e1fdd4b161e24891506844c82b8f
