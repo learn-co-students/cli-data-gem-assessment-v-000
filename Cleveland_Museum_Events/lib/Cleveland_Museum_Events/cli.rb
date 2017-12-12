e83c4e3b400f27b2eb880cf86cfd7dabf380385d
Steven Senkowski <scsenkowski@gmail.com>
2017-12-12 16:17:43 +0000

    coded methods initialize and start

--- Cleveland_Museum_Events/lib/Cleveland_Museum_Events/cli.rb
+++ Cleveland_Museum_Events/lib/Cleveland_Museum_Events/cli.rb
@@ -1,15 +1,22 @@
+class ClevelandMuseumEvents::CLI
 
+def initialize
+  puts "Welcome to your Cleveland Museum's and Garden's event finder!"
+  start
+end
 
-puts "Hi!  Would you like to see the events at Cleveland's Museums and Botanical Gardens? (Y/n)"
-input = gets.strip.downcase
-if input == "y"
-  puts "Would you like to see the events for the day? week? or month? (day/week/month)"
-      date_input = gets.strip.downcase
-        if date_input == "day"
+def start
+  puts "Would you like to see the events at Cleveland's Museums and Botanical Gardens? (Y/n)"
+  input = gets.strip.downcase
+  if input == "y"
+    puts "Would you like to see the events for the day? week? or month? (day/week/month)"
+        date_input = gets.strip.downcase
+          if date_input == "day"
 
-        elsif date_input == "week"
+          elsif date_input == "week"
 
-        elsif date_input == "month"
+          elsif date_input == "month"
 
-        else puts "Please enter a valid command: 'day', 'week' or 'month'"
-        end
+          else puts "Please enter a valid command: 'day', 'week' or 'month'"
+          end
+end
