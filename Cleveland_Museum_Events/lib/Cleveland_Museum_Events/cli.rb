494e9839953ea357ec57742497de6801c5261000
Steven Senkowski <scsenkowski@gmail.com>
2017-12-12 16:11:46 +0000

    Framing the CLI input prompts and commands.  Added an invalid entry statement

---
+++ Cleveland_Museum_Events/lib/Cleveland_Museum_Events/cli.rb
@@ -0,0 +1,15 @@
+
+
+puts "Hi!  Would you like to see the events at Cleveland's Museums and Botanical Gardens? (Y/n)"
+input = gets.strip.downcase
+if input == "y"
+  puts "Would you like to see the events for the day? week? or month? (day/week/month)"
+      date_input = gets.strip.downcase
+        if date_input == "day"
+
+        elsif date_input == "week"
+
+        elsif date_input == "month"
+
+        else puts "Please enter a valid command: 'day', 'week' or 'month'"
+        end
