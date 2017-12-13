fed8debd8efebaffc616c1c986665ba49e7d0013
Steven Senkowski <scsenkowski@gmail.com>
2017-12-13 13:49:25 +0000

    defining the Art Museum Scraper

--- Cleveland_Museum_Events/lib/Cleveland_Museum_Events/Scrapers.rb
+++ Cleveland_Museum_Events/lib/Cleveland_Museum_Events/Scrapers.rb
@@ -1,4 +1,26 @@
-class Art_Museum
+class Art_Museum::Scraper
+
+    def get_page
+      Nokogiri::HTML(open("http://www.clevelandart.org/calendar"))
+    end
+
+    def scrape_events_index
+      self.get_page.css("div.field-name-field-card-title a href").text
+    end
+
+    def make_events
+      scrape_events_index.each do |e|
+        Art_Museum::Scraper.new_from_index_page(e)
+      end
+    end
+
+  end
+
+end
+
+
+
+class Natural_History_Museum::Scraper
 
   def Scraper
 
@@ -8,17 +30,7 @@ end
 
 
 
-class Natural History Museum
-
-  def Scraper
-
-  end
-
-end
-
-
-
-class Botanical_Gardens
+class Botanical_Gardens::Scraper
 
   # Month - h2 "big-calendar-month"
   # Day - class "days font-garamond" text(Day of the Month) a (href)
