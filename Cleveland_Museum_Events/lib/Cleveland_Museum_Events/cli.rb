50ce813a401192e178b3a3309713dafb7fc57fc4
Steven Senkowski <scsenkowski@gmail.com>
2017-12-14 17:23:53 +0000

    correcting gems, environment and console requirements

--- Cleveland_Museum_Events/lib/Cleveland_Museum_Events/cli.rb
+++ Cleveland_Museum_Events/lib/Cleveland_Museum_Events/cli.rb
@@ -1,38 +1,46 @@
-class ClevelandMuseumEvents::CLI
-
-def initialize
-  puts "Welcome to your Cleveland Museum's and Garden's event finder!"
-  start
-end
+class CLI
+  attr_accessor :title, :date, :time, :link
+  attr_reader :title, :date, :time, :link
 
 
-def start
-  puts "Would you like to see the events for the month? (Y/n)"
-      date_input = gets.strip.downcase
-        if date_input == "y" or "yes"
-          museum
-        elsif date_input == "n" or "no"
-          puts "Goodbye."
-        else puts "Please enter a valid command: 'Yes/Y/y' or 'No/N,n'"
-        end
-end
-
-def museum
-  puts "What Museum would you like events for? (Art Museum(AM)/Botanical Gardens(BG)/Natural History Museum(NHM)/All)"
-      museum = get.strip.downcase
-      if museum == "art museum" || "am"
-        print_events(am)
-      elsif museum == "botanical gardens" || "bg"
-        print_events(bg)
-      elsif museum == "natural history museum" || "nhm"
-        print_events(nhm)
-      elsif museum == "all"
-        print_events(all)
-      else puts "Please enter a valid command: 'Art Museum(AM)/Botanical Gardens(BG)/Natural History Museum(NHM)/All/Esc (to exit the program)'"
-      end
-end
+    def initialize
+      puts "Welcome to your Cleveland Museum's and Garden's event finder!"
+      start
+    end
 
 
-def print_events(Museum)
+    def start
+      puts "Would you like to see the events for the month? (Y/n)"
+          date_input = gets.strip.downcase
+            if date_input == "y" or "yes"
+              museum
+            elsif date_input == "n" or "no"
+              puts "Goodbye."
+            else puts "Please enter a valid command: 'Yes/Y/y' or 'No/N,n'"
+            end
+    end
 
-end
+    def museum
+      puts "What Museum would you like events for? (Art Museum(AM)/Botanical Gardens(BG)/Natural History Museum(NHM)/All)"
+          input = gets.strip.downcase
+          if museum == "art museum" || "am"
+              Art_Museum::Scrapers.new_from_index_page(e)
+            print_events(am)
+          elsif museum == "botanical gardens" || "bg"
+            Botanical_Gardens::Scrapers.new_from_index_page(e)
+            print_events(bg)
+          elsif museum == "natural history museum" || "nhm"
+            Natural_History_Museum::Scrapers.new_from_index_page(e)
+            print_events(nhm)
+          elsif museum == "all"
+            print_events(all)
+          else puts "Please enter a valid command: 'Art Museum(AM)/Botanical Gardens(BG)/Natural History Museum(NHM)/All/Esc (to exit the program)'"
+          end
+    end
+
+
+    def list_events(Museum)
+      puts
+    end
+
+  end
---
+++ Cleveland_Museum_Events/lib/Cleveland_Museum_Events/scrapers.rb
@@ -0,0 +1,66 @@
+class Scrapers
+
+      class Art_Museum
+    attr_accessor :title, :date, :time, :link
+
+        def open_page
+          doc = Nokogiri::HTML(open("http://www.clevelandart.org/calendar"))
+        end
+
+        def scrape_events_index
+          self.open_page.css("#div.field-name-field-card-title a href").text
+          binding.pry
+        end
+
+        def make_events
+          scrape_events_index.each do |e|
+            Art_Museum::Scrape.new_from_index_page(e)
+          end
+        end
+
+      end
+
+
+
+    class Natural_History_Museum
+      attr_accessor :title, :date, :time, :link
+
+      def open_page
+        doc = Nokogiri::HTML(open("https://www.cmnh.org/visit/calendar"))
+      end
+
+      def scrape_events_index
+        self.open_page.css("span.title-of-summary").text
+        binding.pry
+      end
+
+      def make_events
+        scrape_events_index.each do |e|
+          Natural_History_Museum::Scrape.new_from_index_page(e)
+        end
+      end
+
+    end
+
+
+
+    class Botanical_Gardens
+      attr_accessor :title, :date, :time, :link
+
+        def open_page
+          doc = Nokogiri::HTML(open("https://www.cbgarden.org/calendar-of-events.aspx"))
+        end
+
+        def scrape_events_index
+          self.open_page.css("td.days a href").text
+          binding.pry
+        end
+
+        def make_events
+          scrape_events_index.each do |e|
+            Botanical_Gardens::Scrape.new_from_index_page(e)
+          end
+        end
+
+
+end
