5505fe35b62730507da451e6576142c1d7ccf95e
Steven Senkowski <scsenkowski@gmail.com>
2017-12-14 17:52:56 +0000

    Editing namespace and version modules

--- Cleveland_Museum_Events/lib/Cleveland_Museum_Events/version.rb
+++ Cleveland_Museum_Events/lib/Cleveland_Museum_Events/version.rb
@@ -1,13 +1,3 @@
-module Cleveland
-<<<<<<< HEAD
-
-      VERSION = "0.1.0"
-    end
-=======
-  module Museum
-    module Events
-      VERSION = "0.1.0"
-    end
-  end
+module ClevelandMusuemEvents
+  VERSION = "0.1.0"
 end
->>>>>>> ddd398c8c550e1fdd4b161e24891506844c82b8f
