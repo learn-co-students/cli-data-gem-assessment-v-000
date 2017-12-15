0ade339d4f18b5ba0b22fb1511425cdea9c135c2
Blakeley Bond <burrick2003@yahoo.com>
2017-12-15 03:56:11 +0000

    Done.

--- bin/gsdsearch.rb
+++ bin/gsdsearch.rb
@@ -1,8 +1,8 @@
 require_relative '../config/environment'
 
-url2="http://www.magsr.org/maleadopt.htm"
-url="http://www.magsr.org/femaleadopt.htm"
+url2="https://www.magsr.org/maleadopt.htm"
+url="https://www.magsr.org/femaleadopt.htm"
 Dog.create_from_webpage(url)
 Dog.create_from_webpage(url2)
-#Dog.magsr_fixer
+#Dog.magsr_fixer(Dog.all)
 Dog.save_output
--- lib/Dog_search_session.rb
+++ lib/Dog_search_session.rb
@@ -1,52 +1,93 @@
-class Dog_search_session # change to DogSearchSession
+class DogSearchSession # change to DogSearchSession
 attr_accessor :selectors, :loaded, :selected_dogs
+
   def initialize
     @selectors = []
     @loaded = "Not Loaded"
     @selected_dogs = []
   end#initialize
-  def self.loaded? #sets status value inside @loaded, yes/no site scrape has been completed
+
+  def loaded? #sets status value inside @loaded, yes/no site scrape has been completed
     if Dog.all.size == 0
       @loaded = "Not Loaded Yet"
     elsif Dog.all.size > 0
       @loaded = "Success! #{Dog.all.size} Dogs Loaded"
+    end
+    #binding.pry
   end #self.loaded?
 
   def self.cls
     system("clear")
   end #self.clear
 
-  def self.start
-    Dog_search_session.loaded?
+  def run
+    loaded?
     main_screen
-  end
+  end #self.start
 
   def load_dogs
-    url2="http://www.magsr.org/maleadopt.htm"
-    url="http://www.magsr.org/femaleadopt.htm"
+    url2="https://www.magsr.org/maleadopt.htm"
+    url="https://www.magsr.org/femaleadopt.htm"
     Dog.create_from_webpage(url)
     Dog.create_from_webpage(url2)
     #Dog.magsr_fixer
     Dog.save_output
+    loaded?
+    main_screen
+  end #load_dogs
+
+  def session_exit
+    DogSearchSession.cls
+    puts "Adopt Today!  Have a nice day."
+    exit
   end
 
   def main_screen
-    Dog_search.cls
+    DogSearchSession.cls
     puts "Welcome to Mid-Atlantic German Shepherd Rescue Dog Search Main Menu"
     puts " Please select from the following:"
     puts "1) Load the dogs from MAGSR.org.  Current Status: #{@loaded}"
     puts "2) Begin Dog Search"
     puts "3) Exit"
-    puts "4) "
-    main_input = user_input(3)
+    puts "4) View All Dogs"
+    #binding.pry
+    main_input = user_input(4)
       if main_input == 1
         load_dogs
         main_screen
       elsif main_input == 2
         search_screen
+      elsif main_input == 3
+        session_exit
+      elsif main_input == 4
+        all_dogs_display
       end
-
   end #main_screen
+
+  def all_dogs_display
+    DogSearchSession.cls
+    Dog.all.each do |dog|
+      DogSearchSession.cls
+      puts "Name: #{dog.name}"
+      puts "Gender: #{dog.gender}"
+      puts "Breed: #{dog.breed}"
+      puts "Age: #{dog.age}"
+      puts "Color: #{dog.color}"
+      puts "Size: #{dog.size}"
+      puts "Housebroken: #{dog.housebroken}"
+      puts "Obedience Trained: #{dog.obedience}"
+      puts "Living Situation: #{dog.location}"
+      puts "Good with Kids? #{dog.kids}"
+      puts "Good with Cats? #{dog.cats}"
+      puts "Good with other dogs? #{dog.otherdogs}"
+      puts "MAGSR ID#: #{dog.id}"
+      puts "Image Link: #{dog.image_url}"
+      puts "Description: #{dog.description}"
+      puts "Press Any Key To Continue"
+      gets.strip
+
+    end
+  end
 
   def user_input(num_choices) #works
     input = 0
@@ -55,10 +96,10 @@ attr_accessor :selectors, :loaded, :selected_dogs
       input = gets.strip.to_i
     end
     input
-  end
+  end #user_input
 
   def search_screen
-    Dog_search.cls
+    DogSearchSession.cls
     puts "Welcome to the Search Menu.  There are #{Dogs.all.size} dogs available, and #{@selected_dogs.size} matches."
     puts
     puts "Please select from the following:"
