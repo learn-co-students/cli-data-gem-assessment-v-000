133f0e95b874b7e99d6e6daf62112e1be7ab05d7
Miwa <paintfx@gmail.com>
2017-12-21 18:10:23 +0000

    Done.

--- coffee_roasters/lib/coffee_roasters.rb
+++ coffee_roasters/lib/coffee_roasters.rb
@@ -1,3 +1,3 @@
 require_relative "./coffee_roasters/version"
-require_relative './coffee_roasters/roasters'
+require_relative './coffee_roasters/roaster'
 require_relative './coffee_roasters/cli'
--- coffee_roasters/lib/coffee_roasters/roaster.rb
+++ coffee_roasters/lib/coffee_roasters/roaster.rb
@@ -1,4 +1,5 @@
 class CoffeeRoasters::Roaster
+  attr_accessor :name, :location, :bean, :description, :url
 
   def self.all
     puts <<~HEREDOC
@@ -7,12 +8,14 @@ class CoffeeRoasters::Roaster
     HEREDOC
     roaster_1 = self.new
     roaster_1.name = "Blue Bottle"
+    roaster_1.location = "SF"
     roaster_1.bean = "Oakland Lights, a fruity and spicy holiday blend of a wet processed Sumatra and natural Ethiopian"
     roaster_1.description = "In the 15 or so years since Blue Bottle launched in an alley that they described as smelling like a bathroom..."
     roaster_1.url = "https://bluebottlecoffee.com/store/oakland-lights"
 
     roaster_2 = self.new
     roaster_2.name = "Camber"
+    roaster_2.location = "BELLINGHAM, WA"
     roaster_2.bean = "Honduras Migdoneo Enamorado, grown in the highlands around Lake Yojoa with a slower development time resulting in falvors of cinnamon, mandarin orange, and honey"
     roaster_2.description = "Q Graders are the sommeliers of the coffee world, trained to detect the most imperceptible defects and differences in beans."
     roaster_2.url = "http://cambercoffee.com/#two"
