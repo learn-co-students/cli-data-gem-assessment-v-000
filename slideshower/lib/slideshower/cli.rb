fedf71f0d6c11fdcea9faf7c690b626bc10a7a42
Nolan Byrnes <nolanehs@gmail.com>
2017-12-21 14:53:56 +0000

    Done.

--- slideshower/lib/slideshower/cli.rb
+++ slideshower/lib/slideshower/cli.rb
@@ -8,7 +8,11 @@ def call
 end #of call
 
 def search
+<<<<<<< HEAD
   puts "Welcome to Slideshower! Please type the kind of stock photos you would like to see. "
+=======
+  puts "Welcome to Slideshower! Please type the kind of pictures you would like to see. "
+>>>>>>> 34143a7b33fb4dede501889a00f1784350c75711
   puts "Pictures of:"
   @search = Slideshower::Search.search
 end
@@ -20,9 +24,15 @@ end
 def menu
   input = nil
   while input != "exit"
+<<<<<<< HEAD
   puts "Press 1 to view the stock photos in a slideshow."
   puts "Press 2 to view all of the stock photos in a list"
   puts "Press 3 to choose a new stock photo subject."
+=======
+  puts "Press 1 to view the photos in a slideshow."
+  puts "Press 2 to view all of the photos in a list"
+  puts "Press 3 to choose a new photo subject."
+>>>>>>> 34143a7b33fb4dede501889a00f1784350c75711
   puts "Type 'exit' to exit the program."
   input = gets.strip
 
