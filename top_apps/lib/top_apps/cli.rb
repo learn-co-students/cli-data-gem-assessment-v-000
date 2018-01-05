e0be4e1fc46b70eb2f76bbbd5b95d11d9c5bf647
Brittany Morris <brittmmendez@gmail.com>
2018-01-05 23:09:53 +0000

    rough draft of CLI and started app.rb

--- top_apps/lib/top_apps/cli.rb
+++ top_apps/lib/top_apps/cli.rb
@@ -4,6 +4,7 @@ class TopApps::CLI
     introduction
     list_categories
     pick_category
+    exit_cli
   end
 
   def introduction
@@ -26,29 +27,56 @@ class TopApps::CLI
   end
 
   def pick_category
-    puts ""
-    puts "Type the number for the category that you would like to see the top apps for or exit:"
     input =""
     while input != "exit"
+      puts "Type the number for the category that you would like to see the top apps for, type 'list' to see the categories again, or exit:"
       input=gets.strip.downcase
 
       case input
       when "1"
-        puts "best_app"
+        puts "Best App"
+        pick_app
       when "2"
-      puts "  entertaining"
+        puts "entertaining"
+        pick_app
       when "3"
-        puts "social"
+        puts "  social"
+        pick_app
       when "4"
-      puts "  daily_helper"
+        puts "daily_helper"
+        pick_app
       when "5"
-      puts "  innovative"
+        puts "innovative"
+        pick_app
       when "6"
-      puts "  hidden_gem"
+        puts "hidden_gem"
+        pick_app
       when "7"
         puts "most_popular"
+        pick_app
+      when "list"
+      list_categories
+    else
+      puts "Invalid number, Type the number for the category that you would like to see the top apps for, type 'list' to see the categories again, or exit:"
       end
     end
   end
+
+      def exit_cli
+      puts "Thank you trying my Gem!  I hope you found a new app to give a try!"
+    end
+
+    def pick_app
+      puts "1.  This is th name of the best_app"
+      puts "2.  This is th name of the best_app"
+      puts "Type the number for the app that you would like to see more information on, 'list' to see the categories again, or exit:"
+      input=gets.strip.downcase
+      if input=="1"
+        puts "here is information about that best app!"
+      elsif input=="2"
+        puts "here is information about the 2nd best app!"
+      end
+    end
+
 
 end
