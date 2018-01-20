86cf7ddb303cf896c7e4d0900eaf608f1da4aa1e
Naomi Dennis <njdennis94@gmail.com>
2018-01-20 08:24:05 +0000

    Done.

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+#git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+gem "pry"
+gem "nokogiri"
+gem "open_uri_redirections"
+gem "require_all"
+gem "rspec"
---
+++ bin/rssme.rb
@@ -0,0 +1,13 @@
+# Created By Naomi Dennis
+# For Flatiron CLI Gem Project -- Ruby
+# January 20th 2018
+#
+# PURPOSE: To view the trending subreddits on www.reddit.com for the current day.
+
+
+#include the library files 
+require_relative "../config/environment.rb"
+
+#Create the display and show the welcome message
+a = Display.new
+a.welcome
