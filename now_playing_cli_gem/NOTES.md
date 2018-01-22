4a41ebc3398b3326566abb104166f6556c5d3893
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-22 21:10:13 +0000

    Hard coding more data

--- now_playing_cli_gem/NOTES.md
+++ now_playing_cli_gem/NOTES.md
@@ -17,6 +17,6 @@ Shows a list of movies
 1. Maze Runner: The Death Cure (2018) - Young hero Thomas embarks on a mission to find a cure for a deadly disease known as the "Flare" - Rosa Salazar, Thomas Brodie-Sangster, Dylan O'Brien, Kaya Scodelario
 2. Hostiles (2017) - In 1892, a legendary Army captain reluctantly agrees to escort a Cheyenne chief and his family through dangerous territory -  Scott Shepherd, Rosamund Pike, Ava Cooper, Stella Cooper
 
-Which movie do you want to learn more about?
+Which movie would you like to learn more about, 1 or 2?
 
 1
--- now_playing_cli_gem/lib/now_playing_cli_gem/cli.rb
+++ now_playing_cli_gem/lib/now_playing_cli_gem/cli.rb
@@ -2,15 +2,20 @@
 class NowPlayingCliGem::CLI
 
   def call
-    puts "Movies Now Playing:"
     list_movies
+    menu
   end
 
   def list_movies
+    puts "Movies Now Playing:"
     puts <<-DOC
       1. Maze Runner: The Death Cure (2018) - Young hero Thomas embarks on a mission to find a cure for a deadly disease known as the "Flare" - Rosa Salazar, Thomas Brodie-Sangster, Dylan O'Brien, Kaya Scodelario
       2. Hostiles (2017) - In 1892, a legendary Army captain reluctantly agrees to escort a Cheyenne chief and his family through dangerous territory -  Scott Shepherd, Rosamund Pike, Ava Cooper, Stella Cooper
     DOC
+  end
+
+  def menu
+    puts "Which movie would you like to learn more about, 1 or 2?"
   end
 
 end
