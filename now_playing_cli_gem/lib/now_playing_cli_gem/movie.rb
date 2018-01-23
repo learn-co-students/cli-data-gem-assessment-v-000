3d8ac57a09f2ee252c182741c407d2b259f12010
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-23 15:09:12 +0000

    Refactoring cli

--- now_playing_cli_gem/lib/now_playing_cli_gem/movie.rb
+++ now_playing_cli_gem/lib/now_playing_cli_gem/movie.rb
@@ -5,11 +5,6 @@ class NowPlayingCliGem::Movie
   def self.today
     # Returning instances of the movies
 
-    # puts <<-DOC
-    #  1. Maze Runner: The Death Cure (2018) - Young hero Thomas embarks on a mission to find a cure for a  deadly disease known as the "Flare" - Rosa Salazar, Thomas Brodie-Sangster, Dylan O'Brien, Kaya Scodelario
-    #  2. Hostiles (2017) - In 1892, a legendary Army captain reluctantly agrees to escort a Cheyenne chief and his family through dangerous territory -  Scott Shepherd, Rosamund Pike, Ava Cooper, Stella Cooper
-    #DOC
-
     movie_1 = self.new
     movie_1.title = "Maze Runner: The Death Cure (2018)"
     movie_1.details = "Young hero Thomas embarks on a mission to find a cure for a deadly disease known as the 'Flare'"
