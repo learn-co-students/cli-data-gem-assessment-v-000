67094fe3e5ce45afc22be152c5dd73ba831a82ef
github username <github email address>
2018-01-23 18:29:30 -0600

    debugging exec file

--- wikihow_tech_topics/lib/wikihow_tech_topics/wikihow_tech_topics.rb
+++ wikihow_tech_topics/lib/wikihow_tech_topics/wikihow_tech_topics.rb
@@ -3,8 +3,9 @@ require_relative '../lib/scraper.rb'
 class WikihowTechTopics
 
     def call
+        puts "Welcome to Wikihow Tech Topics\n"
+        puts "Choose an article title below to read the article. Type the number."
     end
-
 end
     
-    will be called to run the program
+    # will be called to run the program
