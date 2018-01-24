f06227dd199109691e95375564c2480bf82fd986
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-24 19:10:57 +0000

    Finished writing a majority of code - second layer broken

--- now_playing_cli_gem/lib/now_playing_cli_gem/cli.rb
+++ now_playing_cli_gem/lib/now_playing_cli_gem/cli.rb
@@ -2,21 +2,58 @@ class NowPlayingCliGem::CLI
 
 # The program
   def call
-
+    menu
   end
 
 # Scraped data
   def list
-
+    puts ""
+    puts "Movies now playing in theaters:"
+    puts ""
+    NowPlayingCliGem::Movie.all.each.with_index(1) do |movie, i|
+      puts "#{i}. #{movie.name}"
+    end
   end
 
   def print_movie(movie)
+    puts ""
+    puts "#{movie.name}:"
 
+    puts ""
+    puts "Summary: #{movie.summary}"
+    puts ""
+
+    puts "Starring: #{movie.stars}"
+    puts ""
   end
 
 # Interface prompts
   def menu
-
+    list
+    input = nil
+    while input != "exit"
+      puts ""
+      puts "What movie would you more information on? Please type in the name of the movie or the corresponding number."
+      puts ""
+      puts "Enter list to see a list of the movies again."
+      puts ""
+      puts "Enter exit to end the program."
+      puts ""
+      input = gets.strip
+      if input == "list"
+        list
+      elsif input.to_i == 0
+        if movie = NowPlayingCliGem::Movie.find_by_name(input)
+          print_movie(movie)
+        end
+      elsif input.to_i > 0
+        if movie = NowPlayingCliGem::Movie.find(input.to_i)
+          print_movie(movie)
+        end
+      end
+    end
+    puts ""
+    puts "See you next time for more now playing movies!"
+    puts ""
   end
-  
 end
--- now_playing_cli_gem/lib/now_playing_cli_gem/movie.rb
+++ now_playing_cli_gem/lib/now_playing_cli_gem/movie.rb
@@ -2,31 +2,49 @@ class NowPlayingCliGem::Movie
   attr_accessor :name, :summary, :stars
 
   def initialize(name = nil, url = nil)
-      @name = name
-      @url = url
-    end
+    @name = name
+    @url = url
+  end
 
 # Returning scraped data of the movies
-    def self.all
+  def self.all
+    @@all ||= scrape_now_playing
+  end
 
+  def self.find(id)
+    self.all[id-1]
+  end
+
+  def self.find_by_name(name)
+    self.all.detect do |m|
+      m.name.downcase.strip == name.downcase.strip ||
+      m.name.split("(").first.strip.downcase == name.downcase.strip
     end
-
-    def self.find(id)
-
-    end
-
-    def self.find_by_name(name)
-
-    end
+  end
 
 # Scraping summary data
-    def summary
-
-    end
+  def summary
+    @summary ||= plot_summary_doc.search("p.plotsummary").text.strip
+  end
 
 # Scraping stars data
-    def stars
+  def stars
+    @stars ||= doc.search("span[itemprop='actors'] span[itemprop='name']").collect{|e| e.text.strip}.join(", ")
+  end
 
+# Scraping name data
+  private
+    def self.scrape_now_playing
+      doc = Nokogiri::HTML(open('http://www.imdb.com/movies-in-theaters/'))
+      names = doc.search("h4[itemprop='name'] a[itemprop='url']")
+      names.collect{|e| new(e.text.strip, "http://imdb.com#{e.attr("href").split("?").first.strip}")}
     end
 
-end
+    def plot_summary_doc
+      @plot_summary_doc ||= Nokogiri::HTML(open("#{self.url}plotsummary"))
+    end
+
+    def doc
+      @doc ||= Nokogiri::HTML(open(self.url))
+    end
+  end
