84823b62d59d593abd7a2d48d67baf0fb8963144
github username <github email address>
2018-01-23 19:19:29 -0600

    got exec file to work

--- wikihow_tech_topics/lib/wikihow_tech_topics/wikihow_tech_topics.rb
+++ wikihow_tech_topics/lib/wikihow_tech_topics/wikihow_tech_topics.rb
@@ -1,11 +1,11 @@
-require_relative '../lib/scraper.rb'
+# require_relative '../lib/scraper.rb'
 
 class WikihowTechTopics
 
-    def call
-        puts "Welcome to Wikihow Tech Topics\n"
-        puts "Choose an article title below to read the article. Type the number."
-    end
+    # def call
+    #     puts "Welcome to Wikihow Tech Topics\n"
+    #     puts "Choose an article title below to read the article. Type the number."
+    # end
 end
     
     # will be called to run the program
