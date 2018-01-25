c714e7e4061756b8129b2cf37683d109916176d7
github username <github email address>
2018-01-25 14:38:48 -0600

    stubbed more for WikihowTechTopics; need to get some debugging tool to work

--- wikihow_tech_topics/lib/wikihow_tech_topics/wikihow_tech_topics.rb
+++ wikihow_tech_topics/lib/wikihow_tech_topics/wikihow_tech_topics.rb
@@ -3,12 +3,14 @@
 require 'pry'
 
 class WikihowTechTopics
+    binding.pry
 
     attr_accessor :title, :content
 
     @@all = []
 
 need to be able to create a new object from the attributes in hash and access them in class
+how to access hash from Scraper to instantiate here?
 
 
     def initialize(title, content)
@@ -18,12 +20,9 @@ need to be able to create a new object from the attributes in hash and access th
     end
 
     def self.get_scraped_info
-        Scraper.
+    end
 
-        
     def self.all
         @@all
     end
 end
-    
-    # will be called to run the program
