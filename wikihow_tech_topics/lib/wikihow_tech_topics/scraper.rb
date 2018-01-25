c506c81ac794e81ded61847e7f866eaae80deaef
github username <github email address>
2018-01-25 16:39:47 -0600

    building out exec file

--- wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
+++ wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
@@ -15,10 +15,12 @@ binding.pry
 
         do on same page for later
 
-        <div class="thumbnail d-height d-width">
-        <a href="//www.wikihow.com/Change-Alexa%27s-Name">
-        <div id="bodycontents">
-
+        # <div class="thumbnail d-height d-width">
+        # <a href="//www.wikihow.com/Change-Alexa%27s-Name">
+        # <div id="bodycontents">
+    def self.scrape_linked_page_for_content
+        
+        home_page = Nokogiri::HTML(open(https://www.wikihow.com/Category:Computers-and-Electronics))
 
         home_page.css(<div class="thumbnail").each do |article|
     
@@ -28,17 +30,5 @@ binding.pry
         }
 
         @article_hash << articles
-
-
-    #     profile_hash = {}
-    
-    # profile_page = Nokogiri::HTML(open(profile_url))
-    
-    # link = profile_page.css("div.social-icon-container").children.css("a").map { |sm| sm.attribute("href").text }
-    
-    # link.each do |site|
-
-        end
-    end
-    
+    end    
 end
