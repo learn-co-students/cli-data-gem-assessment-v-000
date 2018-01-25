a1865c4e2e64be11cea05e56f92bc16134fc9a70
github username <github email address>
2018-01-25 16:48:31 -0600

    made cli file for lib dir/changed exec file in bin to call new instande of cli file

---
+++ wikihow_tech_topics/lib/wikihow_tech_topics/cli.rb
@@ -0,0 +1,42 @@
+class CLI
+
+    def call
+        puts "Welcome to Wikihow Tech Topics"
+        puts ""
+        puts "Type the number of the article you wish to read."
+        # puts "#{title.index}. #{self.create_article}"
+        
+    end
+
+    def self.create_article
+        make a new instance of scraper
+    end
+
+    def self.title_array
+        iterate over new article and put title keys in an array
+    end 
+
+    def self.user_gets_title
+
+
+        # program gets user input about which number of story to read
+
+        article_number = gets.strip
+
+        # article number is index of article scraped from scraper
+
+        # articles_arr = []
+
+        # articles_arr << scraped_articles 
+        
+        # articles_arr.each_with_index {|article, index| 
+        #     "#{index}. #{article}"
+        
+        # if article_number = index
+        #     put article
+    def display_title
+        access the index of each article title
+        if article_number == index of article title
+            put article content
+    end
+end
