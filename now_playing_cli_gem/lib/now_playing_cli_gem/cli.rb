3e9bf2063ea15915c414fb8444ab031b093d0e73
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-25 18:10:29 +0000

    Continued refactoring and adding in helpful comments

--- now_playing_cli_gem/lib/now_playing_cli_gem/cli.rb
+++ now_playing_cli_gem/lib/now_playing_cli_gem/cli.rb
@@ -1,11 +1,11 @@
 class NowPlayingCliGem::CLI
 
-# The program
+# The program itself
   def call
     menu
   end
 
-# Scraped data
+# Scraped list of now playing movies that is generated when a user run the program or when they type list
   def list
     puts ""
     puts "Movies now playing in theaters:"
@@ -15,6 +15,7 @@ class NowPlayingCliGem::CLI
     end
   end
 
+# Scraped data that is returned when a user enters a number
   def print_movie(movie)
     puts ""
     puts "#{movie.name}:"
@@ -26,29 +27,39 @@ class NowPlayingCliGem::CLI
     puts "Starring: #{movie.stars}"
   end
 
+# Prompts that are regenerated if a user enters an unrecognized number or word
+  def prompts
+    puts ""
+    puts "What movie would you like more information on? Please type in the corresponding number."
+    puts ""
+    puts "Enter list to see a list of the movies again."
+    puts ""
+    puts "Enter exit to end the program."
+    puts ""
+  end
+
+# Prompt that is generated once the user types exit
+  def goodbye
+    puts ""
+    puts "See you next time for more now playing movies!"
+    puts ""
+  end
+
 # Interface prompts
   def menu
     list
     input = nil
     while input != "exit"
-      puts ""
-      puts "What movie would you like more information on? Please type in the corresponding number."
-      puts ""
-      puts "Enter list to see a list of the movies again."
-      puts ""
-      puts "Enter exit to end the program."
-      puts ""
-      input = gets.strip
-      if input == "list"
-        list
-      elsif input.to_i > 0
-        if movie = NowPlayingCliGem::Movie.find(input.to_i)
-          print_movie(movie)
+      prompts
+        input = gets.strip
+        if input == "list"
+          list
+        elsif input.to_i > 0
+          if movie = NowPlayingCliGem::Movie.find(input.to_i)
+            print_movie(movie)
+          end
         end
       end
-    end
-    puts ""
-    puts "See you next time for more now playing movies!"
-    puts ""
+    goodbye
   end
 end
--- now_playing_cli_gem/lib/now_playing_cli_gem/movie.rb
+++ now_playing_cli_gem/lib/now_playing_cli_gem/movie.rb
@@ -6,9 +6,11 @@ class NowPlayingCliGem::Movie
     @url = url
   end
 
-# Returning scraped data of the movies
+# Returning scraped data of the movies piece by piece
   def self.all
-    @@all ||= scrape_now_playing
+    @@all ||= name
+    @@all ||= summary
+    @@all ||= stars
   end
 
   def self.find(id)
@@ -16,20 +18,22 @@ class NowPlayingCliGem::Movie
   end
 
 # Scraping summary data
-  def summary
+  def self.summary
     doc = Nokogiri::HTML(open('http://www.imdb.com/movies-in-theaters/'))
     summary = doc.search("div[itemprop='description']").text.strip
+    summary.collect{|e| new(e.text.strip, "http://imdb.com#{e.attr("href").split("?").first.strip}")}
   end
 
 # Scraping stars data
-  def stars
+  def self.stars
     doc = Nokogiri::HTML(open('http://www.imdb.com/movies-in-theaters/'))
     stars = doc.search("span[itemprop='actors'] span[itemprop='name']").collect{|e| e.text.strip}.join(", ")
+    stars.collect{|e| new(e.text.strip, "http://imdb.com#{e.attr("href").split("?").first.strip}")}
   end
 
 # Scraping name data
   private
-    def self.scrape_now_playing
+    def self.name
       doc = Nokogiri::HTML(open('http://www.imdb.com/movies-in-theaters/'))
       names = doc.search("h4[itemprop='name'] a[itemprop='url']")
       names.collect{|e| new(e.text.strip, "http://imdb.com#{e.attr("href").split("?").first.strip}")}
