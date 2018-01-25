d1fa22e8e717f37cb86a64011951148093db7f7f
github username <github email address>
2018-01-25 12:47:01 -0600

    building out scraper

--- wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
+++ wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
@@ -3,25 +3,29 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
+    binding.pry
 
-    need to set up array or hash for scraped info
+
+    # need to set up array or hash for scraped info
 
     @article_hash = {}
 
-    need to put the url that will be scraped with standard nokogiri code
+
+    # need to put the url that will be scraped with standard nokogiri code
 
     def self.scrape_home_page
         home_page = Nokogiri::HTML(open(https://www.wikihow.com/Category:Computers-and-Electronics))
 
-        need to iterate over home page for specific information to fill out hash
+        # need to iterate over home page for specific information to fill out hash
 
         # <div class="text"><p>How to <br><span>Change Alexa's Name</span></p></div>
 
-        home_page.css('p.span').each do |article|
+        home_page.css('div').each do |article|
     
         articles = {
-            :title => article.css().text
+            :title => article.css('p.span').text
         }
+        end
     end
     
 end
