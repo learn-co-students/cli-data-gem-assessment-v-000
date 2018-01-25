b33305fb4d5b23d7311260edb530653a6beac244
github username <github email address>
2018-01-25 14:23:38 -0600

    edited scraper; need finish, debug, and connect to Wikihow_tech_topics

--- wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
+++ wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
@@ -6,10 +6,17 @@ class Scraper
 binding.pry
     @article_hash = {}
 
-    def self.scrape_home_page
+    def self.scrape_home_page_for_title
         home_page = Nokogiri::HTML(open(https://www.wikihow.com/Category:Computers-and-Electronics))
 
         # <div class="text"><p>How to <br><span>Change Alexa's Name</span></p></div>
+
+        do on same page for later
+
+        <div class="thumbnail d-height d-width">
+        <a href="//www.wikihow.com/Change-Alexa%27s-Name">
+        <div id="bodycontents">
+
 
         home_page.css('div').each do |article|
     
@@ -18,7 +25,16 @@ binding.pry
         }
 
         @article_hash << articles
-        
+
+
+    #     profile_hash = {}
+    
+    # profile_page = Nokogiri::HTML(open(profile_url))
+    
+    # link = profile_page.css("div.social-icon-container").children.css("a").map { |sm| sm.attribute("href").text }
+    
+    # link.each do |site|
+
         end
     end
     
--- wikihow_tech_topics/lib/wikihow_tech_topics/wikihow_tech_topics.rb
+++ wikihow_tech_topics/lib/wikihow_tech_topics/wikihow_tech_topics.rb
@@ -11,10 +11,16 @@ class WikihowTechTopics
 need to be able to create a new object from the attributes in hash and access them in class
 
 
-    def initialize
-        
+    def initialize(title, content)
+        @title = title
+        @content = content  
+        @@all << self
     end
-end
+
+    def self.get_scraped_info
+        Scraper.
+
+        
     def self.all
         @@all
     end
