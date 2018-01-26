3f743aca91bb3d5474d143effba2d5210fa3ce4d
github username <github email address>
2018-01-26 12:09:17 -0600

    working out exec file

--- wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
+++ wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
@@ -7,23 +7,24 @@ require 'pry'
 
 class Scraper
 
-    article_array = []
+
+#     article_array = []
     
-    def self.scraped_title_and_content(article_hash)
-        home_page = Nokogiri::HTML(open(https://www.wikihow.com/Category:Computers-and-Electronics))
+#     def self.scraped_title_and_content(article_hash)
+#         home_page = Nokogiri::HTML(open(https://www.wikihow.com/Category:Computers-and-Electronics))
         
-        content_url = Nokogiri::HTML(open(https://www.wikihow.com/Category:Computers-and-Electronics))
+#         content_url = Nokogiri::HTML(open(https://www.wikihow.com/Category:Computers-and-Electronics))
 
-        home_page.css(<div class="thumbnail").each do |article|
+#         home_page.css(<div class="thumbnail").each do |article|
     
-        articles = {
-            :title => article.css('p.span').text
-            :content_url => article.css( <a href="//www.wikihow.com/Change-Alexa%27s-Name">).text
-        }
+#         articles = {
+#             :title => article.css('p.span').text
+#             :content_url => article.css( <a href="//www.wikihow.com/Change-Alexa%27s-Name">).text
+#         }
 
-        article_array << articles
-    end
-    article_array    
+#         article_array << articles
+#     end
+#     article_array    
 end
 
         # <div class="text"><p>How to <br><span>Change Alexa's Name</span></p></div>
