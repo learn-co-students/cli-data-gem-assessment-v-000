c14c379674f795cb78bd4f648310d9b4c11061b4
github username <github email address>
2018-01-25 22:22:47 -0600

    worked out scraper.rb with example array and stubbed exec file

--- wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
+++ wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
@@ -3,13 +3,12 @@ require 'nokogiri'
 require 'pry'
 
 
-test_array = [{'title' => 'how to text', 'content' => 'just do it'}], {'title' => 'how to email', 'content' => 'just do it, too'}]
+# test_array = [{'title' => 'how to text', 'content' => 'just do it'}], {'title' => 'how to email', 'content' => 'just do it, too'}]
 
 class Scraper
 
     article_array = []
     
-
     def self.scraped_title_and_content(article_hash)
         home_page = Nokogiri::HTML(open(https://www.wikihow.com/Category:Computers-and-Electronics))
         
@@ -22,9 +21,9 @@ class Scraper
             :content_url => article.css( <a href="//www.wikihow.com/Change-Alexa%27s-Name">).text
         }
 
-        article_hash << articles
+        article_array << articles
     end
-    article_hash    
+    article_array    
 end
 
         # <div class="text"><p>How to <br><span>Change Alexa's Name</span></p></div>
