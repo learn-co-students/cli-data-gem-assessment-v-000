744cf870fff9ee279cbe63019e8b0e8fc353bbdf
github username <github email address>
2018-01-25 18:06:45 -0600

    deleted cli.rb and wikihow_tech_topics.rb and starting over from basics

--- wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
+++ wikihow_tech_topics/lib/wikihow_tech_topics/scraper.rb
@@ -4,12 +4,12 @@ require 'pry'
 
 class Scraper
 
-    @article_hash = {}
+    article_hash = {}
 
-    def self.scrape_home_page_for_title_and_content
+    def self.scraped_title_and_content(article_hash)
         home_page = Nokogiri::HTML(open(https://www.wikihow.com/Category:Computers-and-Electronics))
         
-        congtent_url = Nokogiri::HTML(open(https://www.wikihow.com/Category:Computers-and-Electronics))
+        content_url = Nokogiri::HTML(open(https://www.wikihow.com/Category:Computers-and-Electronics))
 
         home_page.css(<div class="thumbnail").each do |article|
     
@@ -18,8 +18,9 @@ class Scraper
             :content_url => article.css( <a href="//www.wikihow.com/Change-Alexa%27s-Name">).text
         }
 
-        @article_hash << articles
-    end    
+        article_hash << articles
+    end
+    article_hash    
 end
 
         # <div class="text"><p>How to <br><span>Change Alexa's Name</span></p></div>
