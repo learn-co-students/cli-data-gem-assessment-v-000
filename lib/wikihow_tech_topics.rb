871bc14cc8ec3d5ee3fde9b14e33ffdefeed8af0
github username <github email address>
2018-01-26 19:55:36 -0600

    no major changes besides putting module block at top of lib/wikihow_tech_topics; started working on lib/wikihow_tech_topics/wikihow_tech_topics.rb

--- lib/wikihow_tech_topics.rb
+++ lib/wikihow_tech_topics.rb
@@ -1,6 +1,10 @@
+module WikihowTechTopics
+end
+
 require './config/environment.rb'
 
 # require 'wikihow_tech_topics/version.rb'
 
-module WikihowTechTopics
-end
+
+
+
--- lib/wikihow_tech_topics/wikihow_tech_topic.rb
+++ lib/wikihow_tech_topics/wikihow_tech_topic.rb
@@ -1,2 +1,6 @@
 class WikihowTechTopics::WikihowTechTopic
+
+    attr_accessor :title, :content
+
+    
 end
