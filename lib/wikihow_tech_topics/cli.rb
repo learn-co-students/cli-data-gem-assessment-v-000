53e5af0f6b9037e076b44c0c2b7e53dfcf3e8aa6
github username <github email address>
2018-01-31 16:35:33 -0600

    working out cli

--- lib/wikihow_tech_topics/cli.rb
+++ lib/wikihow_tech_topics/cli.rb
@@ -9,27 +9,25 @@ class WikihowTechTopics::CLI
     end
 
 
-    # binding.pry
-
-    # title_input = gets.strip
-
-    # if title_input == i
-    #     run_content
-    # end
 
     def start
         puts "Welcome to Wikihow Tech Topics!"
         puts ""
         puts "Review the list of articles above then \ntype the number of the article above \nthat you wish to read."
         puts ""
+        end
     end
-    
+
     def self.scraped_titles
         newly_scraped_for_titles = WikihowTechTopics::Scraper.scraped_title_ary
 
         newly_scraped_for_titles.each.with_index(1) do |title, i|
             unless title == ""
-            puts "#{i}. #{title}"
+                puts "#{i}. #{title}"
+                title_input = gets.strip
+                if title_input == i
+                    run_content
+        
             end      
         end
         puts ""
@@ -38,10 +36,11 @@ class WikihowTechTopics::CLI
     
     scraped_titles
 
+    def run_content
+        
 
 
-
-    def self.run_content
+        
         newly_scraped_for_content = WikihowTechTopics::Scraper.scraped_content_array
 
         newly_scraped_for_content.each do |content_steps|
