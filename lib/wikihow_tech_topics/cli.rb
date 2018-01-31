9d5f73791dc9bfe60477cc2c20e54b484b58e0ef
github username <github email address>
2018-01-31 16:08:14 -0600

    getting cli to work

--- lib/wikihow_tech_topics/cli.rb
+++ lib/wikihow_tech_topics/cli.rb
@@ -3,32 +3,36 @@ require 'pry'
 
 class WikihowTechTopics::CLI
 
-    def self.scraped_titles
+    def call
+        start
+    end
+
+    def scraped_titles
         newly_scraped_for_titles = WikihowTechTopics::Scraper.scraped_title_ary
 
         newly_scraped_for_titles.each.with_index(1) do |title, i|
             puts "#{i}. #{title}"
            
-        title_input = gets.strip
-
-            if title_input == i
-                run_content
-            end
         end
     end
     # binding.pry
 
-    def call
-        start
-    end
+    # title_input = gets.strip
+
+    # if title_input == i
+    #     run_content
+    # end
+
+
+
 
     def start
         puts "Welcome to Wikihow Tech Topics"
         puts ""
         puts "Type the number of the article you wish to read."
         puts ""
-        scraped_titles
     end
+    scraped_titles
 
     def self.run_content
         newly_scraped_for_content = WikihowTechTopics::Scraper.scraped_content_array
