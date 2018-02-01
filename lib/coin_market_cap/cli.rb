d937d221fb7a16a7a75a69773434205e774a97c6
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-01 20:40:54 +0000

    market class file created

--- lib/coin_market_cap/cli.rb
+++ lib/coin_market_cap/cli.rb
@@ -2,23 +2,7 @@ class CoinMarketCap::CLI
 
   def call
 
-    puts "Welcome to the CoinMarketCap list Gem"
-    puts "Type list to display the top 15 cryptocurrencies then choose the cryptocurrency:"
-
-    input = nil
-
-    while input == nildsds
-
-      input = gets.strip
-
-      if input == "list"
-        puts "list of crypto"
-      elsif input == "exit"
-        break
-      end
-
-    end
-
+    puts "Hello World"
 
   end
 
