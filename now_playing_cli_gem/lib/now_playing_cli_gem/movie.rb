09c0c60b1eb43ea2cecb64b3abb5639593615f92
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-02-01 18:24:23 +0000

    Needs fine tuning

--- now_playing_cli_gem/lib/now_playing_cli_gem/movie.rb
+++ now_playing_cli_gem/lib/now_playing_cli_gem/movie.rb
@@ -1,13 +1,26 @@
 class NowPlayingCliGem::Movie
-  attr_accessor :name, :link
 
+# Automatically sets up getter and setter methods for the instance variables
+  attr_accessor :name, :link
+  attr_reader :url
+
+  # def name
+  #   @name
+  # end
+
+  # def name=(name)
+  #   @name = name
+  # end
+
+# Instance variables - making them accessible to other outside methods
   def initialize(name = nil, link = nil, url = nil)
     @name = name
     @link = link
     @url = url
   end
 
-# Returning scraped data of the movies piece by piece
+# Returning scraped data of the movies piece by piece when prompted by the user
+# Need help with what exactly this peiece of code does
   def self.all
     @@all ||= name
   end
@@ -25,8 +38,9 @@ class NowPlayingCliGem::Movie
 
     def self.link
       doc = Nokogiri::HTML(open('http://www.imdb.com/movies-in-theaters/'))
+      binding.pry
       link = doc.search("a[itemprop='url']").collect{|e| new(e.text.strip, "http://imdb.com#{e.attr("href").split("?").first.strip}")}
     end
-  end
+end
 
 #PROGRAM IS WORKING AND COMPLETE
