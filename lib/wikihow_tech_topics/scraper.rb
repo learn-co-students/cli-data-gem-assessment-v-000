c1471fdcb577eb1fabdc2552a25d5538dff777ef
github username <github email address>
2018-01-31 20:43:53 -0600

    finished recoding (not refactoring) Scraper, ready for debugging

--- lib/wikihow_tech_topics/scraper.rb
+++ lib/wikihow_tech_topics/scraper.rb
@@ -42,15 +42,17 @@ class WikihowTechTopics::Scraper
 
         # link = profile_page.css("div.social-icon-container").children.css("a").map { |sm| sm.attribute("href").text }
     
-        link = home_page.css(".thumbnail").children.css("a").map { |content_link| content_link.attribute("href").text }
+        content_urls = home_page.css(".thumbnail").children.css("a").map { |content_link| content_link.attribute("href").text }
         # css('div.steps')
-        link..each do |full_content|
-            full_content.css("b").text
-            @content_array << full_content.css("b").text
+        content_urls.map do |complete_content_url| 
+            content_pages_to_scrape = Nokogiri::HTML(open(complete_content_url))}
+        
+        @content_array = content_pages_to_scrape.css('div.steps').map {|full_content|
+                full_content.css("b").text
+        
+        @content_array
+        
             binding.pry
-
-                # end
-        end
     end
 end
 
