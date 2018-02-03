7277f526d109b203c2c922f4311fb544df18b201
Michael Pope <popemray1986@gmail.com>
2018-02-03 21:21:43 +0000

    Done.

--- nba_rosters/lib/nba_rosters/cli.rb
+++ nba_rosters/lib/nba_rosters/cli.rb
@@ -1,7 +1,27 @@
-class Nba_Rosters::CLI
+class NbaRosters::CLI
 
   def call
-    puts "Nba teams roster"
+    teams
+    list_roster
+  end
+
+  def teams
+    puts "Enter the number of team you'd like to see the roster of:"
+    puts "list of teams"
+    input = nil
+    while input != "exit"
+    input = gets.strip.downcase
+      case input
+      when "1"
+        list_roster
+      when "2"
+        list_roster
+      end
+    end
+  end
+
+  def list_roster
+    puts "list of roster"
   end
 
 end
