2b50678f602671209ba42b6e6382b3d71320e19a
Katrina Brinson <kbrinso@gmail.com>
2018-02-10 22:45:44 +0000

    Done.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+gem 'nokogiri', '~> 1.8', '>= 1.8.2'
+gem 'pry', '~> 0.11.3'
+gem 'rspec', '~> 3.7'
---
+++ lib/portals_scraper.rb
@@ -0,0 +1,17 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+
+class Portals
+  attr_accessor 
+  # all_portals_url = "https://en.wikipedia.org/wiki/Portal:Contents/Portals#Technology_and_applied_sciences"
+  # {"Tech" title="Portal:Technology", 
+  # "Computing" title="Portal:Information technology",
+  #   "Social sciences" title="Portal:Social sciences",
+  #   "People & Self" =?,
+    
+  #   "Astronomy" = 
+  #   ""
+  # }
+end
