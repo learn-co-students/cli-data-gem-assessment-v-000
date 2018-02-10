e5f0e08dd326af89825c86a76cdb673812b409e8
github username <github email address>
2018-02-09 18:02:28 -0600

    making Scraper depend on the model rather than the model depend on Scraper

--- lib/wikihow_tech_topics/scraper.rb
+++ lib/wikihow_tech_topics/scraper.rb
@@ -14,10 +14,14 @@ class WikihowTechTopics::Scraper
         url = "https://www.wikihow.com/Category:Selecting-and-Buying-a-Computer"
         home_page = Nokogiri::HTML(open(url))
         content_url_array = []
-        content_urls = home_page.css(".thumbnail").children.css("a").map { |content_link| content_link.attribute("href").text }
+        # content_urls = home_page.css(".thumbnail").children.css("a").map { |content_link| content_link.attribute("href").text }
+        content_urls = home_page.css(".thumbnail").children.css("a").map { |content_link| content_link.text }
+
         http_added_to_content_urls = content_urls.map { |content_url| "https:" + content_url }
         http_added_to_content_urls.pop(4)
         http_added_to_content_urls
+        binding.pry
+
     end
 
     def self.get_titles_from_content_urls
