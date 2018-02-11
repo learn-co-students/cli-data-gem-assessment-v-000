babcf5c829feff8bc3e1db47ed265014262f4fc8
Michael Pope <popemray1986@gmail.com>
2018-02-11 20:38:42 +0000

    Done.

--- nba_rosters/lib/nba_rosters/list_teams.rb
+++ nba_rosters/lib/nba_rosters/list_teams.rb
@@ -2,13 +2,16 @@ class NbaRosters::Team
 
   def self.list
     #prints out list of all nba teams
-    teams_names []
+    team_names []
 
-    teams_names << self.scrape_teams
+    team_names << self.scrape_teams
+
+    team_names
   end
 
   def self.scrape_teams
     doc = Nokogiri::HTML(open('http://www.nba.com/teams'))
+    teams = doc.search("div.team__list_wrapper").text
     binding.pry
   end
 end
