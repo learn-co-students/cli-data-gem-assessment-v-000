10d794a9022b42c84c4b51d9c090efb9ed0bdc50
Aubrey Neimeier <neimeier.1@osu.edu>
2018-02-14 16:19:37 +0000

    stubbed out print_cat_recipes in Scaper class

--- easy_vegan/lib/easy_vegan/scraper.rb
+++ easy_vegan/lib/easy_vegan/scraper.rb
@@ -1,12 +1,14 @@
 class EasyVegan::Scraper
 
-  def print_cat_recipes
+  def print_cat_recipes(input)
     <<-HEREDOC
     1. Vegan Cinnamon Rolls
     2. 5-ingredient vegan Dark Chocolate Bars
     3. 5-ingredient Vegan Caramel Sauce
     4. Creamy Avocado Banana Green Smoothie
     HEREDOC
+
+    puts "#{input}"
   end
 
 end
