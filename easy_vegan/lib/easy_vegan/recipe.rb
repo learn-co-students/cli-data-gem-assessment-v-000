3bd8a2d915abf65edf7b13dddc927e656d3f7ffb
Aubrey Neimeier <neimeier.1@osu.edu>
2018-02-14 19:46:34 +0000

    developing scrape index page method

--- easy_vegan/lib/easy_vegan/recipe.rb
+++ easy_vegan/lib/easy_vegan/recipe.rb
@@ -1,12 +1,25 @@
 class EasyVegan::Recipe
 attr_accessor :category, :total_time, :cuisine_category, :serving_size, :url, :title
 
+  def initialize
+    @@all = []
+  end
+
+
   def self.titles(category_number)
     @titles = []
     @titles << EasyVegan::Scraper.titles(category_number)
   end
 
+  def self.all
+    @@all
+  end
 
+
+
+
+#we need to create Recipe objects and shovel in their title and url.
+#we need to go to each ojbect, using their url attribute and then scrape remaining attribute
 
 
 end
--- easy_vegan/lib/easy_vegan/scraper.rb
+++ easy_vegan/lib/easy_vegan/scraper.rb
@@ -1,17 +1,27 @@
 class EasyVegan::Scraper
 
   def self.titles(category_number)
-    doc = Nokogiri::HTML(open("https://minimalistbaker.com/recipe-index/"))
-    binding.pry
-
-
-
     <<-HEREDOC
     1. Vegan Cinnamon Rolls
     2. 5-ingredient vegan Dark Chocolate Bars
     3. 5-ingredient Vegan Caramel Sauce
     4. Creamy Avocado Banana Green Smoothie
     HEREDOC
+    self.scrape_index_page
+  end
+
+
+  def self.scrape_index_page
+    doc = Nokogiri::HTML(open("https://minimalistbaker.com/recipe-index/"))
+    doc.each_with_index do |category, index|
+      binding.pry
+      category.each do |x|
+        scraped_recipes << {
+        :title => x.css("article a").attr("title").value,
+        :url => x.css("article a").attr("href").value
+      }
+      end
+    end
   end
 
 
