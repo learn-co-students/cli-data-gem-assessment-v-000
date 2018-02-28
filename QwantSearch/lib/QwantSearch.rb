cb4d44a5f922f0b68954e63acd102ba7994bc3ed
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-28 12:47:49 +0000

    Done.

--- QwantSearch/lib/QwantSearch.rb
+++ QwantSearch/lib/QwantSearch.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 require "./QwantSearch/version"
+=======
+require "QwantSearch/version"
+>>>>>>> 8f6c4c4c35ed77c9de333090f57b59b709ac443d
 
 module QwantSearch
   # Your code goes here...
