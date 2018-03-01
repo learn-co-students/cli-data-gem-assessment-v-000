adf3a3993b1ab0e3aedc84dd4292dc6c1092a7ca
Bertrand Chevalier <33043853+Bechev@users.noreply.github.com>
2018-03-01 06:18:10 -0500

    Add files via upload

---
+++ QwantSearch/lib/QwantSearch.rb
@@ -0,0 +1,9 @@
+require_relative "./QwantSearch/version"
+require_relative "./QwantSearch/cli"
+require_relative "./QwantSearch/scrapper"
+require 'pry'
+
+
+module QwantSearch
+  # Your code goes here...
+end
