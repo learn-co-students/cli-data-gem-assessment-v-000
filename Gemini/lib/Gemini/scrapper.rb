f42f3b6c0cbc4f3110fce90e5def6cd7b743a6d5
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-02 23:51:22 +0000

    Done.

--- Gemini/lib/Gemini/scrapper.rb
+++ Gemini/lib/Gemini/scrapper.rb
@@ -6,7 +6,33 @@ require_relative '../Gemini'
     end
 
     def scrap_website
+      @doc = Nokogiri::HTML(open("http://nakamotoinstitute.org/literature/"))
+      @document_name =[]
+      @document_author =[]
+      @document_date =[]
       puts "scrapping website"
+      scrap_table
+      binding.pry
+    end
+
+    def scrap_table
+      table = @doc.at('table')
+
+      table.search('tr').each do |tr|
+        column_count = 0
+        tr.search('td').each do |td|
+          case (column_count % 4)
+          when 0
+            @document_name << td.text.strip
+          #when 1 - not used as we don't collect formats
+          when 2
+            @document_author << td.text.strip
+          when 3
+            @document_date << td.text.strip
+          end
+          column_count +=1
+        end
+      end
     end
 
     def display_pairs
