9f26f9871a9e072dd701093f27a62a2a6968e7ee
Katrina Brinson <kbrinso@gmail.com>
2018-03-06 01:17:27 +0000

    attempting to figure out a better selector for links body

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,47 +11,68 @@ class Scraper
     doc = Nokogiri::HTML(html) do |config|
       config.noblanks
     end
+    binding.pry
+    #"display: block;border: 0px solid #A3BFB1; #if use this I need to remove the first 2 undesired elements
+
+    # ####################determining headline containers###################
+    # doc.search("#mw-content-text div table table div").each{|anchor|
+    #   if anchor['style'] == "position: relative;border: 0px solid #A3BFB1;background: #CEF2E0;color: black;padding: .1em;text-align: center;font-weight: bold;font-size: 100%;margin-bottom: 0px;border-top: 1px solid #A3BFB1;border-bottom: 1px solid #A3BFB1;"
+    #     anchor['class'] = "title_container" unless anchor.text.include?("General reference")
+    #   end
+    #   #if anchor.children.css.include?("#selected_topic")
+    # }
+    #
+    # #set .headlines class for the topic_selection
+    # doc.search("h2 .mw-headline big").each{|anchor|
+    #   anchor['class'] = "headlines" unless anchor.text == "Wikipedia's contents: Portals" || anchor.text == "Wikipedia's contents: Portals" || anchor.text.include?("General reference")
+    # }
+    #
+    # #updating the @@all_topics hash with topic symbols
+    # doc.search(".headlines").each{|anchor|
+    #   copy = anchor.text.chomp("(see in all page types)").strip
+    #   copy.slice!(-3..-1)
+    #   @@all_topics << copy
+    # }
+    # binding.pry
+    #
+    # ####################################
 
     #Set div container class .title_container for all parent elements of each .headlines title
-
-    doc.search("#mw-content-text div table table div").each{|anchor|
-      if anchor['style'] == "position: relative;border: 0px solid #A3BFB1;background: #CEF2E0;color: black;padding: .1em;text-align: center;font-weight: bold;font-size: 100%;margin-bottom: 0px;border-top: 1px solid #A3BFB1;border-bottom: 1px solid #A3BFB1;"
-        anchor['class'] = "title_container" unless anchor.text.include?("General reference")
-      end
-      binding.pry
-      #if anchor.children.css.include?("#selected_topic")
+    doc.search("#mw-content-text div table").each{|anchor|
+      #if anchor['style'] == "position: relative;border: 0px solid #A3BFB1;background: #CEF2E0;color: black;padding: .1em;text-align: center;font-weight: bold;font-size: 100%;margin-bottom: 0px;border-top: 1px solid #A3BFB1;border-bottom: 1px solid #A3BFB1;"
+        anchor['class'] = "title-container" unless anchor.text.include?("General reference")
+      #end
     }
 
     #set .headlines class for the topic_selection
     doc.search("h2 .mw-headline big").each{|anchor|
       anchor['class'] = "headlines" unless anchor.text == "Wikipedia's contents: Portals" || anchor.text == "Wikipedia's contents: Portals" || anchor.text.include?("General reference")
       if anchor.text == topic_selection
-        anchor['id'] = "selected_topic"
+        anchor['id'] = "selected-topic"
       end
     }
-
     #removes line break element from doc object #Text('/n')
     #sets the parent class container .portal_container for each of the .portals
-    doc.search(".title_container").each{|anchor|
-      anchor.next.remove
+    doc.search(".title-container").each{|anchor|
+      #anchor.next.remove
       listcontent = anchor.next
-      if anchor.at_css("#selected_topic") != nil
-        listcontent['class'] = "portal_container"
+      if anchor.at_css("#selected-topic") != nil
+        listcontent['class'] = "portal-container"
       end
     }
 
     #finds all portal links under the selected_topic and adds them to the @topic_links array
-    doc.search(".portal_container p b a").each{|anchor|
+    doc.search(".portal-container p b a").each{|anchor|
       if anchor.attribute("href").value.include?("/wiki/Portal:")
         @topic_links << anchor.attribute("href").value.prepend("https://en.wikipedia.org")
-        binding.pry
         #anchor['class']="portals"
       end
     }
-    doc.search(".portal_container dl dd a").each{|anchor|
+    doc.search(".portal-container dl dd a").each{|anchor|
       #anchor['class']="portals"
       @topic_links << anchor.attribute("href").value.prepend("https://en.wikipedia.org")
     }
+    binding.pry
     return @topic_links
    end
 ######################
