a4dcbbcd224fe87c71753ffe21f3dde458cef6d6
Branden Mosley <branden.mosley1337@gmail.com>
2018-03-08 22:33:55 +0000

    added cli.rb and updated file requirements

--- lib/shooting_match_finder.rb
+++ lib/shooting_match_finder.rb
@@ -3,3 +3,5 @@ require "shooting_match_finder/version"
 module ShootingMatchFinder
   # Your code goes here...
 end
+
+require 'shooting_match_finder/cli'
---
+++ lib/shooting_match_finder/cli.rb
@@ -0,0 +1,7 @@
+class ShootingMatchFinder::CLI
+
+  def start
+    puts "You've started the program! There's no going back!"
+  end
+
+end
