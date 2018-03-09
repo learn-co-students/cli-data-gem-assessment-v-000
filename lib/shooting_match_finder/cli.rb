c10504b3a1cc18c8ff0253f1a4ef5b04b57741ad
Branden Mosley <branden.mosley1337@gmail.com>
2018-03-09 23:40:40 +0000

    Built out framework for #menu

--- lib/shooting_match_finder/cli.rb
+++ lib/shooting_match_finder/cli.rb
@@ -16,11 +16,22 @@ class ShootingMatchFinder::CLI
   end
 
   def menu
-
+    puts "Enter the number of the match you'd like more info about, or type exit."
+    input = nil
+    while input != "exit"
+      input = gets.strip
+      if input.to_i == 0
+        puts "Please enter a match number."
+      elsif input.to_i == 1
+        puts "1. 2018 Berry's Steel Open - Date"
+      elsif input.to_i == 2
+        puts "2. SUPS Practical Pistol - Date"
+      end
+    end
   end
 
   def farewell
-
+    puts "Come back again for more matches!"
   end
 
 end
