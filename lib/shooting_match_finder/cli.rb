cdd7cd0adb37341ea32bf96a59d67b727acba4ed
Branden Mosley <branden.mosley1337@gmail.com>
2018-03-09 23:28:34 +0000

    Started work on #list_matches

--- lib/shooting_match_finder/cli.rb
+++ lib/shooting_match_finder/cli.rb
@@ -1,7 +1,26 @@
 class ShootingMatchFinder::CLI
 
   def start
-    puts "You've started the program! There's no going back!"
+    list_matches
+    menu
+    farewell
+  end
+
+  def list_matches
+    puts "Here are the matches in your area:"
+    puts <<-DOC
+    1. 2018 Berry's Steel Open - Date
+    2. SUPS Practical Pistol - Date
+    3. Other match - date
+    DOC
+  end
+
+  def menu
+
+  end
+
+  def farewell
+
   end
 
 end
