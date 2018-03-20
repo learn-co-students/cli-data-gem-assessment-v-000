f2d448733bd2a0597e8fe9ba56d205b16bf93e0a
Branden Mosley <branden.mosley1337@gmail.com>
2018-03-20 18:31:11 +0000

    Created scraper.rb to contain the Scraper class.

---
+++ lib/shooting_match_finder/scraper.rb
@@ -0,0 +1,9 @@
+class ShootingMatchFinder::Scraper
+
+  def self.scrape_matches #This will go away once I build Scraper since all this logic will be encapsulated there. This is just a test hash builder method.
+  match_details = {:name => "2018 Berry's Steel Open",
+                   :date => "Sometime in April",
+                   :location => "SUPS Range, St. George, UT"}
+  end
+
+end
