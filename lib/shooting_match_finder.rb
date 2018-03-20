4474faf5eaee516fd73eb4ffc486342e12750092
Branden Mosley <branden.mosley1337@gmail.com>
2018-03-20 18:29:48 +0000

    Created scraper.rb to contain the Scraper class.

--- lib/shooting_match_finder.rb
+++ lib/shooting_match_finder.rb
@@ -1,3 +1,4 @@
 require "shooting_match_finder/version"
 require 'shooting_match_finder/cli'
 require 'shooting_match_finder/match'
+require 'shooting_match_finder/scraper'
--- lib/shooting_match_finder/match.rb
+++ lib/shooting_match_finder/match.rb
@@ -3,26 +3,20 @@ class Match
 
   @@all = []
 
-  def self.scrape_matches #This will go away once I build Scraper since all this logic will be encapsulated there. This is just a test hash builder method.
-  match_details = {:name => "2018 Berry's Steel Open",
-                   :date => "Sometime in April",
-                   :location => "SUPS Range, St. George, UT"}
-  end
 
   def initialize
     @@all << self
   end
 
-  def self.new_from_practiscore #creates Matches and assigns them attributes based on a hash. Will call the Scraper class
+  def self.new_from_practiscore #creates Matches and assigns them attributes based on a hash.
     new_match = self.new
-    match_details = self.scrape_matches #this will change to Scraper.scrape_matches once I've built the Scraper class.
+    match_details = Scraper.scrape_matches #this will change to Scraper.scrape_matches once I've built the Scraper class.
     new_match.add_attributes(match_details)
   end
 
   def add_attributes(match_details_hash)
     match_details_hash.each {|key, value| self.send(("#{key}="), value)}
   end
-
 
   def self.show_matches
     @@all
