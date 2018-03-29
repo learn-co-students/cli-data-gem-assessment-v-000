95b4ede0e26783eb5f2cc1e4177d3bbba45c2695
Branden Mosley <branden.mosley1337@gmail.com>
2018-03-29 17:45:46 +0000

    Removed BASE_PATH constant from Scraper.rb, as it was unecessary.

--- lib/shooting_match_finder/scraper.rb
+++ lib/shooting_match_finder/scraper.rb
@@ -3,8 +3,6 @@ require 'nokogiri'
 
 class ShootingMatchFinder::Scraper
 
-  BASE_PATH = "https://practiscore.com"
-
   def self.scrape_matches(website_url) #This creates an array of hashes that we then use to create match objects.
     doc = Nokogiri::HTML(open(website_url))
     matches = doc.css(".list-group-item")
@@ -16,7 +14,6 @@ class ShootingMatchFinder::Scraper
 
   def self.scrape_from_match_url(match_url) #Used to add attributes to newly created match objects.
     doc = Nokogiri::HTML(open(match_url))
-    #This logic is used to identify matches based on whether or not registration is open, since they need to scrape differently.
     if !doc.xpath('//*[@id="psMainContainer"]/div[2]/div[3]/div[1]/p[1]').text.include?("Registration opens")
       match_info = {:match_start => doc.xpath('//*[@id="psMainContainer"]/div[2]/div[3]/div[1]/p[1]/strong').text.strip,
                     :location => doc.xpath('//*[@id="psMainContainer"]/div[2]/div[3]/div[1]/p[2]').text.gsub("Location:", "").strip,
