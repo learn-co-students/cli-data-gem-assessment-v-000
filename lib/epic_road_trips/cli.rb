88cd6b573ebbb08cac7e5ac1c976597b0d64a512
Dee Race <deerace12@gmail.com>
2018-03-29 19:22:20 +0000

    Adding major project folders

---
+++ lib/epic_road_trips/cli.rb
@@ -0,0 +1,12 @@
+class EpicRoadTrips::CLI
+
+  def call
+
+  end
+
+
+  def list_road_trips
+    puts "Let's go on an EPIC road trip!"
+  end
+
+  
---
+++ lib/epic_road_trips/trips.rb
@@ -0,0 +1,8 @@
+class EpicRoadTrips::Trips
+  attr_accessor :name, :position, :road, :country, :description
+
+
+
+
+
+end
