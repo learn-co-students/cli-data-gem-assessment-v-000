f5e7511821ed42c12d13ef46166289748c49acf3
Trevor Seitz <trevinla@gmail.com>
2018-04-10 13:46:25 +0000

    articles are being created but scraping is sporatic

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,29 +2,34 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  # attr_accessor :doc, :article_title, :article_date, :article_text, :article_url, :comments_url
 
-  def self.scrape_news_page(news_url)
-  article_array = []
-  article_hash = {}
-  html = open(news_url)
-  doc = Nokogiri::HTML(html)
-  # binding.pry
 
-  doc.css(".blog-post").each do |article|
+  def initialize(site_url)
+    @doc = Nokogiri::HTML(open(site_url))
+    parse(@doc)
+    # @doc.css(".blog-post").each do |article|
+    #   article_title = article.css(".blog-link").first.text,
+    #   article_date = article.css(".date-text").text.gsub("\n\t\t", "").gsub("\n\t", ""),
+    #   article_text = article.css(".paragraph").text,
+    #   article_url = article.css(".blog-title-link blog-link").text, #find href for article
+    #   comments_url = article.css(".blog-comments a @href").first.value,
+    #   # binding.pry
+    #   Article.new(article_title, article_date, article_text, article_url, comments_url)
+    # end
     # binding.pry
-    article_hash = {
-      :title => article.css(".blog-title-link").text,
-      :date => article.css(".blog-date").text,
-      # :description => article.css(".blog-title-link").text,
-      :story => article.css(".paragraph").text,
-      :comments_url => article.css(".blog-comments a @href").first.value,
-    }
-    binding.pry
-    article_array << article_hash
+  end
 
+  def parse(nokogiri_file)
+    nokogiri_file.css(".blog-post").each do |article|
+      @article_title = article.css(".blog-title a").first.text,
+      @article_info = article.css(".blog-date .date-text").first.children.text.gsub("\n\t\t", "").gsub("\n\t", ""),
+      @article_text = article.css(".paragraph").text,
+      @article_url = article.css(".blog-link @href").first.value, #find href for article
+      @comments_url = article.css(".blog-comments a @href").first.value,
+      binding.pry
+      Article.new(@article_title, @article_info, @article_text, @article_url, @comments_url)
     end
-binding.pry
-    article_array
   end
 
 end
