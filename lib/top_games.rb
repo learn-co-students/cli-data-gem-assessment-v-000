92e3de1f5e0db5bd78e30669a0e2b5711cd3ad3b
David Ritchey <ritchey0713@gmail.com>
2018-04-19 18:59:11 +0000

    Done.

--- lib/top_games.rb
+++ lib/top_games.rb
@@ -1,6 +1,7 @@
 # using this as the enviroment setup 
 require "nokogiri"
 require 'pry'
+require 'open-uri'
 
 require_relative "top_games/version"
 require_relative "top_games/cli"
--- lib/top_games/scrape.rb
+++ lib/top_games/scrape.rb
@@ -5,32 +5,62 @@ class TopGames::Scrape
    def self.scrape_games
      
      games = []
-        game_1 = self.new 
-    game_1.name = "Chrono Trigger"
-    game_1.developer = "Square"
-    game_1.release_date = "1995"
-    game_1.url = "http://www.ign.com/lists/top-100-rpgs/1/1/3"
-    game_1.summary = "blah blah blah"
+     games << self.scrape_ign
+     
+     
+    #     game_1 = self.new 
+    # game_1.name = "Chrono Trigger"
+    # game_1.developer = "Square"
+    # game_1.release_date = "1995"
+    # game_1.url = "http://www.ign.com/lists/top-100-rpgs/1/1/3"
+    # game_1.summary = "blah blah blah"
     
-    game_2 = self.new 
-    game_2.name = "Final Fantasy VI"
-    game_2.developer = "Square"
-    game_2.release_date = "1994"
-    game_2.url = "http://www.ign.com/lists/top-100-rpgs/1/1/3"
-    game_2.summary = "blah blah blah"
+    # game_2 = self.new 
+    # game_2.name = "Final Fantasy VI"
+    # game_2.developer = "Square"
+    # game_2.release_date = "1994"
+    # game_2.url = "http://www.ign.com/lists/top-100-rpgs/1/1/3"
+    # game_2.summary = "blah blah blah"
     
-    game_3 = self.new 
-    game_3.name = "Baldur's Gate II: Shadows of Amn"
-    game_3.developer = "Bio Wares"
-    game_3.release_date = "2000"
-    game_3.url = "http://www.ign.com/lists/top-100-rpgs/1/1/3"
-    game_3.summary = "blah blah blah"
+    # game_3 = self.new 
+    # game_3.name = "Baldur's Gate II: Shadows of Amn"
+    # game_3.developer = "Bio Wares"
+    # game_3.release_date = "2000"
+    # game_3.url = "http://www.ign.com/lists/top-100-rpgs/1/1/3"
+    # game_3.summary = "blah blah blah"
     
     games 
   end 
   
-  def scrape_ign
+  def self.scrape_ign
+    doc = Nokogiri::HTML(open("https://www.ign.com/lists/top-100-rpgs"))
+    binding.pry
+
+    
     
   end 
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 end 
