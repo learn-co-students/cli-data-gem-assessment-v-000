200c48080eee0396affded85139c03231a064b8c
David Ritchey <ritchey0713@gmail.com>
2018-04-19 17:49:33 +0000

    Done.

--- lib/top_games.rb
+++ lib/top_games.rb
@@ -1,9 +1,11 @@
-
 # using this as the enviroment setup 
+require "nokogiri"
+require 'pry'
 
 require_relative "top_games/version"
 require_relative "top_games/cli"
 require_relative "top_games/games"
 require_relative "top_games/scrape"
+
 
 
