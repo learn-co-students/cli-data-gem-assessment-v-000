6243a71a5da5f453ebed6d060acefa43e18a720c
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-04-22 02:25:28 +0000

    Done.

--- upcoming_movies/lib/upcoming_movies/cli.rb
+++ upcoming_movies/lib/upcoming_movies/cli.rb
@@ -10,18 +10,21 @@ class UpcomingMovies::CLI
   def list_movies
     puts "Listed Below Are Upcming Movies"
     @movies  =  UpcomingMovies::Movies.today
-    puts @movies
+    @movies.each.with_index(1)   do |movies, i|
+      puts "#{i},. #{movies.name} - #{movies.price} #{movies.url}"
+    end
   end
 
   def menu
     puts "Enter the number of movie you want more information on or type exit to "
     input = nil
     while input != "exit"
-      puts "enter the number of movie you want to find more detail on."  
+      puts "enter the number of movie you want to find more detail on."
       input = gets.strip.downcase
-      
+
       if input.to_i > 0
-         puts @movies[input.to_i - 1 ]
+         the_movies =  @movies[input.to_i - 1 ]
+         puts "#{the_movies.name} - #{the_movies.price} #{the_movies.url}"
       elsif input == "list"
         list_movies
       else
