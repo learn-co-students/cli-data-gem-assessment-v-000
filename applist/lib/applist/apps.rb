2bc49d3ba19ef9d6e466d605536155b58ad847d8
Everett <everett.bowline@gmail.com>
2018-05-18 01:30:13 +0000

    Done.

--- applist/lib/applist/apps.rb
+++ applist/lib/applist/apps.rb
@@ -1,5 +1,5 @@
 class Applist::Apps
-  attr_accessor :name, :category, :link
+  attr_accessor :name, :category, :link, :desc, :avg_rating
   
   def self.popular
     app_page = Nokogiri::HTML(open("https://www.apple.com/itunes/charts/free-apps/"))
@@ -7,18 +7,17 @@ class Applist::Apps
     
     app_page.css("div.section-content li").each do |app|
       
-      obj = { name: '', category: '', link: '' }
-      obj[:name] = [app.css("h3 a").text]
-      obj[:category] = [app.css("h4 a").text]
-      obj[:link] = [app.css("a").attribute("href").value]
-      apps << obj
+      app_obj = { name: '', category: '', link: '', desc: '', avg_rating: '' }
+      app_obj[:name] = [app.css("h3 a").text]
+      app_obj[:category] = [app.css("h4 a").text]
+      app_obj[:link] = [app.css("a").attribute("href").value]
+      apps << app_obj
     end
     
     apps.each do |app|
-      
       doc = Nokogiri::HTML(open(app[:link][0]))
-      #doc.css('.desc')
-      binding.pry
+      app[:desc] = doc.css(".section__description").text
+      app[:avg_rating] = doc.css(".we-customer-ratings__averages__display").text
     end
     
   end
