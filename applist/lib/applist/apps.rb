9803e6cf8edc4591028592e5ab5d54bbafadcef2
Everett <everett.bowline@gmail.com>
2018-05-18 01:50:49 +0000

    Done.

--- applist/lib/applist/apps.rb
+++ applist/lib/applist/apps.rb
@@ -18,7 +18,6 @@ class Applist::Apps
     #  doc = Nokogiri::HTML(open(app[:link][0]))
     #  app[:desc] = doc.css(".section__description").text
     #  app[:avg_rating] = doc.css(".we-customer-ratings__averages__display").text
-      
     #binding.pry
     #end
     apps
--- applist/lib/applist/cli.rb
+++ applist/lib/applist/cli.rb
@@ -1,27 +1,31 @@
 class Applist::CLI
-  attr_accessor :name, :category, :link
+  attr_accessor :name, :category, :link, :desc, :avg_rating
   
   def call
-    list_apps
     menu
+    list_apps
     goodbye
   end
   
   def list_apps
     @apps = Applist::Apps.popular
     @apps.each.with_index(1) do | app, i |
-      puts "#{i}. #{app.name} in #{app.category}."
+      puts "#{i}. #{app[:name][0]} in #{app[:category][0]}."
     end
   end
 
   def menu
     puts "This is a list of the most popular free apps available on the Apple App Store. Please enter the number for the app you would like to view. Type list to see the list again or type exit to leave:"
+    list_apps
     input = nil
     while input != "exit"
     input = gets.strip.downcase
       if input.to_i > 0
         the_app = @apps[input.to_i - 1]
-        puts "#{the_app.name} in #{the_app.category}."
+        puts "#{the_app[:name][0]} in #{the_app[:category][0]}."
+        puts "Average Rating: #{the_app[:avg_rating][0]}"
+        puts "Description: #{the_app[:desc][0]}."
+        puts "Please type the number of the app you are interested in, list to see them again, or exit to leave."
       elsif input == "list"
         list_apps
       else
