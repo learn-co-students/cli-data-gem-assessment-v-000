5a50a7c2a46b01d5e674b34b4786cb6709683a58
Heidi Engstrom <hengstrom25@gmail.com>
2018-06-16 14:23:42 +0000

    Done.

---
+++ lib/cli.rb
@@ -0,0 +1,23 @@
+class Simple_Collection::CLI
+  
+  #CLI directives:
+  # 1. Greets user
+  # 2. Offers options
+  # 3. Collects user's input
+  # 4. 
+
+   puts "Hello Knitter! What would you like to make today?"
+   puts "For Accessories, enter 1. For Garments, enter 2."
+   input = gets.strip
+   if input == "1"
+     
+     
+    elsif input == "2" 
+     
+  def goodbye
+    puts "Happy Knitting!"
+  end
+  
+end
+   
+  
---
+++ lib/simple_collection.rb
@@ -0,0 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "./simple_collection/version"
+require_relative './simple_collection/deal'
+require_relative './simple_collection/cli'
