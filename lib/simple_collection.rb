3e53587e8217b6387e9b4ebe9aa810022e8ebfbb
Heidi Engstrom <hengstrom25@gmail.com>
2018-06-16 14:28:25 +0000

    Done.

--- lib/simple_collection.rb
+++
@@ -1,7 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
-
-require_relative "./simple_collection/version"
-require_relative './simple_collection/deal'
-require_relative './simple_collection/cli'
