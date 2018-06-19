40d5fdb7531c114ee3aa02b8e451d1d2798b8828
Heidi Engstrom <hengstrom25@gmail.com>
2018-06-19 01:45:30 +0000

    for erika

--- simple_collection/lib/simple_collection/cli.rb
+++ simple_collection/lib/simple_collection/cli.rb
@@ -2,17 +2,36 @@
 class SimpleCollection::CLI
 
   def call
-    welcome
-    goodbye
-  end
-
-  def welcome
     puts "Welcome Knitter! What would you like to make today?"
-    puts ""
+    start
   end
 
-  def goodbye
-    puts "Happy Knitting!"
+  def start
+    puts "For garments, please enter 1"
+    puts "For accessories, please enter 2"
+    puts "To view all patterns, please enter 3"
+    input = gets.strip.to_i
+
+    #sourcing code goes here
+
+    puts "Which pattern would you like more information on?"
+    input = gets.strip
+
+    #more code here
+
+    #more code here
+
+    puts "Would you like to learn about another pattern? Please enter y or n."
+
+    input = gets.strip
+    if input == "y"
+      start
+    elsif input == "n"
+      puts "Happy Knitting!"
+    else
+      puts "Please enter your answer again."
+      start
+    end
   end
 
 
