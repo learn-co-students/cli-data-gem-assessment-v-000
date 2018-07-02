d7d41140eae63f97d7720fb6ab0f4953a792efeb
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-02 15:26:58 +0000

    add scraper

--- simple_collection/lib/simple_collection.rb
+++ simple_collection/lib/simple_collection.rb
@@ -2,4 +2,10 @@ module SimpleCollection
 
 end
 
-require_relative './simple_collection'
+#require_relative './simple_collection'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require "bundler/setup"
+require_relative '../lib/simple_collection/cli'
+require_relative '../lib/simple_collection/patterns'
--- simple_collection/lib/simple_collection/patterns.rb
+++ simple_collection/lib/simple_collection/patterns.rb
@@ -4,18 +4,6 @@ class Pattern
 
   @@all = []
 
-  def scrape_pattern
-    doc = Nokogiri::HTML(open("http://tincanknits.com/#{url}"))
-    #puts doc.css("p")
-    #stuff = doc.css("tr td a img.img-thumbnail-tight")
-    @size = doc.css("div.LHbox table")[1].css("tr")[0].css("td")[1].text
-    # test size = doc.css("div.LHbox table")[1].css("tr")[0]
-    @materials = doc.css("div.LHbox table")[1].css("tr")[1].css("td")[1].text
-    @gauge = doc.css("div.LHbox table")[1].css("tr")[2].css("td")[1].text
-    @suggested_needles = doc.css("div.LHbox table")[1].css("tr")[3].css("td")[1].text
-    @notes = doc.css("div.LHbox table")[1].css("tr")[4].css("td")[1].text
-    @description = doc.css("div.LHbox blockquote p").text
-  end
 
   def initialize(name=nil, url=nil)
     @name = name
