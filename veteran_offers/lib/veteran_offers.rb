1078f9a73f984486af0e94744d43188dc70a4243
Jos Castro <joselycas@gmail.com>
2018-07-31 17:39:57 +0000

    automatically backed up by learn

---
+++ veteran_offers/lib/veteran_offers.rb
@@ -0,0 +1,8 @@
+#this file acts as the environment
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+require_relative "./veteran_offers/version"
+require_relative './veteran_offers/offer'
+# require_relative './veteran_offers/scraper'
+require_relative './veteran_offers/cli'
