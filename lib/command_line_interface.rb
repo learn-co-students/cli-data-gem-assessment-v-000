7fda4a169e0de921c2095daab53f61046e3aebb9
Katrina Brinson <kbrinso@gmail.com>
2018-08-01 06:35:04 +0000

    refactored CLI for reusable methods

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -6,19 +6,19 @@ class CommandLineInterface
   def self.run
     puts "Welcome to Did-You-Know Wikipedia Edition!"
     puts "Please select a topic to be given a random Wikipedia Portal to read:"
+    get_inputs
+
+  end
+
+  def self.get_inputs
 
     #Displays all available main topics
     display_all_topics
     puts "Select a number to explore that topic"
-    @choice = @list[gets.strip.to_i - 1]
-
+    get_choice
+    get_rand_url
     #finds or creates an Topic instance
-    @topic = Topic.find_or_create_by_name(@choice)
-    @randurl = Scraper.scrape_portals_page(@choice)
 
-    @portal = Portal.find_or_create_by_url(@randurl)
-    @portal.topic = @topic
-    @topic.portals << @portal
 
     puts "We've selected " + Scraper.get_portal_name(@randurl)
  + " for you within the " + @choice +" topic you selected."
@@ -26,6 +26,14 @@ class CommandLineInterface
     if gets.strip == "Y"
       puts @randurl
       Launchy.open(@randurl)
+    else
+      puts "Either type 'reroll' to choose another page within the " + @choice + " topic you selected. Or select a new topic with 'new'."
+      choice = gets.strip
+      if choice == "reroll"
+        get_rand_url
+      elsif choice == "new"
+        get_inputs
+      end
     end
   end
 
@@ -37,5 +45,17 @@ class CommandLineInterface
       @list << item
       puts "#{indx + 1}. #{item}".colorize(colors[indx])
     }
+  end
+
+  def self.get_choice
+    @choice = @list[gets.strip.to_i - 1]
+    @topic = Topic.find_or_create_by_name(@choice)
+  end
+
+  def self.get_rand_url
+    @randurl = Scraper.scrape_portals_page(@choice)
+    @portal = Portal.find_or_create_by_url(@randurl)
+    @portal.topic = @topic
+    @topic.portals << @portal
   end
 end
