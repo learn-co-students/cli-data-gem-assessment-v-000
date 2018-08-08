f02e4d0f87d05641988684096dda35d7459c7e4c
Sushi Chanrai <sushi@pathworks.co>
2018-08-08 10:40:20 +0000

    Done.

--- scrape_test.rb
+++ scrape_test.rb
@@ -17,30 +17,38 @@ class Scraper
     :details_link => events.css("a")[1].attribute("href").value,
     :date => events.css(".date").text.split("- ")[0].strip
   }
-    #date(this only returns the first one though)  =events.css(".date").text.split("- ")[0].strip
-    #date (this returns the same with regular expression) = doc.css(".panel-picture-content .date").text.match(/[A-Z].+\s\d/)
-    #date (this returns dates without location but with \n and its one long string) = doc.css(".panel-picture-content .date").text.strip.gsub(/\s+-.+$/,"")
     end
     #puts "#{scraped_index}"
     scraped_index
   end
+  #date(this only returns the first one though)  =events.css(".date").text.split("- ")[0].strip
+  #date (this returns the same with regular expression) = doc.css(".panel-picture-content .date").text.match(/[A-Z].+\s\d/)
+  #date (this returns dates without location but with \n and its one long string) = doc.css(".panel-picture-content .date").text.strip.gsub(/\s+-.+$/,"")
+
+
+
 
   def scrape_details_page
     url_1 ="https://www.startupgrind.com/events/details/startup-grind-san-francisco-presents-ryan-popple-proterra#/"
     url_2 ="https://www.startupgrind.com/events/details/startup-grind-johannesburg-presents-come-party-with-joburgs-vibrant-entrepreneur-community#/"
     scraped_details = {}
     doc = Nokogiri::HTML(open(url_1))
-    binding.pry
-
-    #long_description => doc.css(".event-description").text.strip
-    #time =>  doc.css(".container-inner")[0].text.split("2018")[1].to_s.strip!.delete!("\r\n\\")
+    scraped_details[:long_descrip] = doc.css(".event-description").text.strip
+    scraped_details[:time] = doc.at(".container-inner").children[8].text.strip
+    scraped_details[:address] = doc.at(".container-inner//span[@itemprop = 'name']").children.text + ", " +
+      doc.at(".container-inner//span[@itemprop = 'streetAddress']").children.text + ", " +
+      doc.at(".container-inner//span[@itemprop = 'addressLocality']").children.text + ", " +
+      doc.at(".container-inner//span[@itemprop = 'postalCode']").children.text
+    scraped_details[:speakers] = doc.css(".carousel-inner").children.css("img").map {|y| y.attribute('alt').value}
+    scraped_details
+    end
+    #long_descrip => doc.css(".event-description").text.strip
+    #time =>  doc.at(".container-inner").children[8].text.strip
     #address_title => doc.at(".container-inner//span[@itemprop = 'name']").children.text
     #address_streetaddress => doc.at(".container-inner//span[@itemprop = 'streetAddress']").children.text
     #address_locality => doc.at(".container-inner//span[@itemprop = 'addressLocality']").children.text
     #address_postcode => doc.at(".container-inner//span[@itemprop = 'postalCode']").children.text
-    #speakers
-  end
-
+    #speakers => doc.css(".carousel-inner").children.css("img").map {|y| y.attribute('alt').value}
 
 
 
