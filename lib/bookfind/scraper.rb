86c57e33d1d32f0a7e9ece43fcf8f67b0dff7c6c
Lamia Haider <lamia.haider@gmail.com>
2018-10-08 18:44:21 +0000

    automatically backed up by learn

--- lib/bookfind/scraper.rb
+++ lib/bookfind/scraper.rb
@@ -8,7 +8,9 @@ class Scraper
     booklist = Nokogiri::HTML(open("https://litreactor.com/columns/storyville-3-essential-books-you-should-read-in-every-major-genre/"))
     booklist.css("h2"). each do |gen|
       binding.pry
+    end
   end
+end
   
   # def getgenre
   #   self.getinfo.css("h2")
@@ -19,4 +21,3 @@ class Scraper
   #     binding.pry 
   #   end
   # end
-end
